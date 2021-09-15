2d51549e913f5277ddaedfca683c6ae752b0be64
Pekka Enberg <penberg@chiselstrike.com>
2021-09-15 14:58:09 +0300

    cli: Add 'chisel type define' command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,8 @@
 pub mod parser;
 
 use chisel::chisel_rpc_client::ChiselRpcClient;
-use chisel::StatusRequest;
+use chisel::{FieldDefinition, StatusRequest, TypeDefinitionRequest};
+use std::fs;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -9,6 +10,19 @@ use structopt::StructOpt;
 enum Opt {
     /// Shows information about ChiselStrike server status.
     Status,
+    Type {
+        #[structopt(subcommand)]
+        cmd: TypeCommand,
+    },
+}
+
+#[derive(StructOpt, Debug)]
+enum TypeCommand {
+    /// Define types in the type system.
+    Define {
+        /// Type definition input file.
+        filename: String,
+    },
 }
 
 pub mod chisel {
@@ -25,6 +39,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let response = client.get_status(request).await?.into_inner();
             println!("Server status is {}", response.message);
         }
+        Opt::Type { cmd } => match cmd {
+            TypeCommand::Define { filename } => {
+                let type_system = parser::parse(&fs::read_to_string(filename)?)?;
+                let mut client = ChiselRpcClient::connect("http://localhost:50051").await?;
+                for type_def in &type_system.defs {
+                    let mut field_defs = Vec::default();
+                    for field_def in &type_def.fields {
+                        field_defs.push(FieldDefinition {
+                            name: field_def.name.to_owned(),
+                            r#type: field_def.ty.to_owned(),
+                        });
+                    }
+                    let request = tonic::Request::new(TypeDefinitionRequest {
+                        name: type_def.name.to_owned(),
+                        field_defs: field_defs,
+                    });
+                    let response = client.define_type(request).await?.into_inner();
+                    println!("Type defined: {}", response.message);
+                }
+            }
+        },
     }
     Ok(())
 }
