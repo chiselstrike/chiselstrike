7c83152147b9a3a81c167f69eb4db8124cbbb59c
Pekka Enberg <penberg@chiselstrike.com>
2021-09-15 15:05:32 +0300

    server: Type definition RPC
    This adds a RPC interface for defining types.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,12 @@
+pub mod types;
+
 use chisel::chisel_rpc_server::{ChiselRpc, ChiselRpcServer};
-use chisel::{StatusRequest, StatusResponse};
+use chisel::{StatusRequest, StatusResponse, TypeDefinitionRequest, TypeDefinitionResponse};
+use std::sync::Arc;
 use structopt::StructOpt;
+use tokio::sync::Mutex;
 use tonic::{transport::Server, Request, Response, Status};
+use types::{Type, TypeSystem};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "chiseld")]
@@ -20,8 +25,18 @@ pub mod chisel {
 /// The RPC service provides a Protobuf-based interface for Chisel control
 /// plane. For example, the service has RPC calls for managing types and
 /// endpoints. The user-generated data plane endpoints are serviced with REST.
-#[derive(Debug, Default)]
-pub struct RpcService {}
+#[derive(Debug)]
+pub struct RpcService {
+    type_system: Arc<Mutex<TypeSystem>>,
+}
+
+impl RpcService {
+    pub fn new(type_system: Arc<Mutex<TypeSystem>>) -> Self {
+        RpcService {
+            type_system: type_system,
+        }
+    }
+}
 
 #[tonic::async_trait]
 impl ChiselRpc for RpcService {
@@ -35,13 +50,27 @@ impl ChiselRpc for RpcService {
         };
         Ok(Response::new(response))
     }
+
+    /// Define a type.
+    async fn define_type(
+        &self,
+        request: Request<TypeDefinitionRequest>,
+    ) -> Result<Response<TypeDefinitionResponse>, Status> {
+        let mut type_system = self.type_system.lock().await;
+        let name = request.into_inner().name;
+        type_system.define_type(Type {
+            name: name.to_owned(),
+        });
+        let response = chisel::TypeDefinitionResponse { message: name };
+        Ok(Response::new(response))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Opt::from_args();
-    let chisel = RpcService::default();
-
+    let ts = Arc::new(Mutex::new(TypeSystem::new()));
+    let chisel = RpcService::new(ts);
     Server::builder()
         .add_service(ChiselRpcServer::new(chisel))
         .serve(opt.listen_addr.parse()?)
