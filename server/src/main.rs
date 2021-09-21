bc3801ae63d63ef59a4d968ac06c392633861aba
Pekka Enberg <penberg@chiselstrike.com>
2021-09-21 11:13:08 +0300

    server: Initial dynamic endpoints
    This is initial implementation of dynamic endpoints. When a type is
    defined, an "read all" endpoint is generated.
    
    For example:
    
    % curl -I -X GET localhost:3000/position
    HTTP/1.1 404 Not Found
    content-length: 0
    date: Wed, 22 Sep 2021 10:17:51 GMT
    
    % cargo run --bin chisel type define cli/examples/person.graphql
    Type defined: Person
    Type defined: Position
    
    % curl -I -X GET localhost:3000/position
    HTTP/1.1 200 OK
    content-length: 2
    date: Wed, 22 Sep 2021 10:19:06 GMT
    
    The endpoint logic is implemented in ApiService, which is a small
    wrapper on top of Hyper library. We might want to replace ApiService
    with a more advanced HTTP router or a Radix-tree based matcher:
    
    https://github.com/ibraheemdev/httprouter-rs
    
    https://github.com/ibraheemdev/matchit
    
    But this implementation is sufficient for now.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,14 @@
+#[macro_use]
+extern crate log;
+
+pub mod api;
 pub mod types;
 
+use api::ApiService;
 use chisel::chisel_rpc_server::{ChiselRpc, ChiselRpcServer};
 use chisel::{StatusRequest, StatusResponse, TypeDefinitionRequest, TypeDefinitionResponse};
+use convert_case::{Case, Casing};
+use serde_json::json;
 use std::sync::Arc;
 use structopt::StructOpt;
 use tokio::sync::Mutex;
@@ -11,9 +18,12 @@ use types::{Type, TypeSystem};
 #[derive(StructOpt, Debug)]
 #[structopt(name = "chiseld")]
 struct Opt {
-    /// Server listen address.
+    /// API server listen address.
+    #[structopt(short, long, default_value = "127.0.0.1:3000")]
+    api_listen_addr: String,
+    /// RPC server listen address.
     #[structopt(short, long, default_value = "127.0.0.1:50051")]
-    listen_addr: String,
+    rpc_listen_addr: String,
 }
 
 pub mod chisel {
@@ -25,14 +35,15 @@ pub mod chisel {
 /// The RPC service provides a Protobuf-based interface for Chisel control
 /// plane. For example, the service has RPC calls for managing types and
 /// endpoints. The user-generated data plane endpoints are serviced with REST.
-#[derive(Debug)]
 pub struct RpcService {
+    api: Arc<Mutex<ApiService>>,
     type_system: Arc<Mutex<TypeSystem>>,
 }
 
 impl RpcService {
-    pub fn new(type_system: Arc<Mutex<TypeSystem>>) -> Self {
+    pub fn new(api: Arc<Mutex<ApiService>>, type_system: Arc<Mutex<TypeSystem>>) -> Self {
         RpcService {
+            api,
             type_system: type_system,
         }
     }
@@ -61,6 +72,16 @@ impl ChiselRpc for RpcService {
         type_system.define_type(Type {
             name: name.to_owned(),
         });
+        let path = format!("/{}", name.to_case(Case::Snake));
+        info!("Registered endpoint: '{}'", path);
+        self.api.lock().await.get(
+            &path,
+            Box::new(|| {
+                // Let's return an empty array because we don't do storage yet.
+                let result = json!([]);
+                result.to_string()
+            }),
+        );
         let response = chisel::TypeDefinitionResponse { message: name };
         Ok(Response::new(response))
     }
@@ -68,13 +89,20 @@ impl ChiselRpc for RpcService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
     let opt = Opt::from_args();
+    let api_addr = opt.api_listen_addr.parse()?;
+    let api = Arc::new(Mutex::new(ApiService::new()));
     let ts = Arc::new(Mutex::new(TypeSystem::new()));
-    let chisel = RpcService::new(ts);
-    Server::builder()
-        .add_service(ChiselRpcServer::new(chisel))
-        .serve(opt.listen_addr.parse()?)
-        .await?;
-
+    let rpc = RpcService::new(api.clone(), ts);
+    let rpc_addr = opt.rpc_listen_addr.parse()?;
+    let rpc_task = tokio::spawn(async move {
+        Server::builder()
+            .add_service(ChiselRpcServer::new(rpc))
+            .serve(rpc_addr)
+            .await
+    });
+    let api_task = api::spawn(api.clone(), api_addr);
+    let _ = tokio::try_join!(rpc_task, api_task)?;
     Ok(())
 }
