194c19771ad181daa03d8e1f5fe0c077a73ddf5d
Pekka Enberg <penberg@chiselstrike.com>
2021-09-22 14:10:23 +0300

    Initial command line test suite
    This adds a test case for "chisel status" command with the lit crate.

---
+++ cli/tests/integration_tests.rs
@@ -0,0 +1,39 @@
+extern crate lit;
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use std::process::Command;
+    use std::{env, thread, time};
+
+    fn bin_dir() -> PathBuf {
+        let mut path = env::current_exe().unwrap();
+        path.pop();
+        path.pop();
+        path
+    }
+
+    fn chiseld() -> String {
+        bin_dir().join("chiseld").to_str().unwrap().to_string()
+    }
+
+    fn chisel() -> String {
+        bin_dir().join("chisel").to_str().unwrap().to_string()
+    }
+
+    #[test]
+    fn lit() {
+        let mut cmd = Command::new(chiseld()).spawn().unwrap();
+        // FIXME: Add a proper check that ensures server is running.
+        thread::sleep(time::Duration::from_secs(1));
+        lit::run::tests(lit::event_handler::Default::default(), |config| {
+            config.add_search_path("tests/lit");
+            config.add_extension("lit");
+            config.constants.insert("chisel".to_owned(), chisel());
+        })
+        .expect("Lit tests failed");
+        cmd.kill().unwrap();
+        cmd.wait().unwrap();
+        // FIXME: Kill server if tests fail.
+    }
+}
