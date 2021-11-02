f1ca1da81b407be68c874dccab145b45ee764fb5
Dejan Mircevski <dejan@chiselstrike.com>
2021-11-02 19:04:23 -0400

    Use examples/find.js in integration tests
    This way the tests are DRY and examples tested.
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- cli/examples/find.js
+++ cli/examples/find.js
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: © 2021 ChiselStrike <info@chiselstrike.com>
-async function chisel(req) {
+export default async function chisel(req) {
     let response = "";
     let people = await Chisel.find_all("Person");
     for await (let person of people) {
-        response += person.first_name;
+        response += person.first_name + " " + person.last_name;
         response += " ";
     }
     return new Response(response);
