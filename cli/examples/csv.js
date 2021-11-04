6516e61c23d3087a5dbf63d8dc6855a1bbd26384
Dejan Mircevski <dejan@chiselstrike.com>
2021-11-03 20:07:07 -0400

    A CSV-ingesting endpoint
    Will make a nice aid for demos.
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

---
+++ cli/examples/csv.js
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: © 2021 ChiselStrike <info@chiselstrike.com>
+export default async function chisel(req) {
+    const lines = (await req.text()).split('\n');
+    for (const line of lines) {
+        const r = line.split(',');
+        await Chisel.store('Person', {"first_name": r[0], "last_name": r[1], "age": 100, "human": true, "height": 5.0});
+    }
+    return new Response('ok\n');
+}
