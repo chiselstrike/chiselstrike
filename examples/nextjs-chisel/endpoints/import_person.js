5d2c1b73ad1c503f2d2f0364d350a8eee8384c8b
Pekka Enberg <penberg@chiselstrike.com>
2021-11-08 10:45:31 +0200

    examples/nextjs-chisel: Use Chisel.json() API

--- examples/nextjs-chisel/endpoints/import_person.js
+++ examples/nextjs-chisel/endpoints/import_person.js
@@ -1,17 +1,11 @@
-function makeResponse(status, msg) {
-    let blob = new Blob([JSON.stringify(msg, null, 2)], {type : 'application/json'});
-    let init = { "status" : status , "message" : blob };
-    return new Response(blob, init);
-}
-
 export default async function chisel(req) {
     if (req.method == 'PUT') {
         try {
             await Chisel.store('Person', await req.json());
-            return makeResponse(200, "ok");
+            return Chisel.json("ok");
         } catch (e) {
-            return makeResponse(500, e);
+            return Chisel.json(e, 500);
         }
     }
-    return makeResponse(405, "Only PUT is allowed");
+    return Chisel.json("Only PUT is allowed", 405);
 }
