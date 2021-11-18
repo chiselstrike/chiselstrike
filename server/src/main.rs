7a114708a1e0bcf3c25094630568d4a5d9dc0ada
Glauber Costa <glauber@chiselstrike.com>
2021-11-18 06:22:03 -0500

    prepare for TPC
    In preparation for TPC, move somethings that are global to a run-once
    shared initialization state. Those are things like signal handlers, the
    RPC server, etc.
    
    Conceptually, each thread should have its own API server, and runtime
    state so we don't contend for them (although at first they may be shared
    to speed up development). So they are kept, together with the deno
    initialization, in the per-thread function.
    
    We will create one thread per executor. At this point, we won't be able
    to support more than one, though. More work needs to be done for that.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,6 +5,7 @@ extern crate log;
 
 use anyhow::Result;
 use chisel_server::server;
+use enclose::enclose;
 use nix::unistd::execv;
 use server::DoRepeat;
 use std::env;
@@ -13,10 +14,32 @@ use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args: Vec<CString> = env::args().map(|x| CString::new(x).unwrap()).collect();
+    let mut executors = vec![];
+
     pretty_env_logger::init();
-    if let DoRepeat::Yes = server::run_on_new_localset(server::Opt::from_args()).await? {
-        let exe = env::current_exe()?.into_os_string().into_string().unwrap();
+
+    let args: Vec<CString> = env::args().map(|x| CString::new(x).unwrap()).collect();
+    let (tasks, shared) = server::run_shared_state(server::Opt::from_args()).await?;
+    let exe = env::current_exe()?.into_os_string().into_string().unwrap();
+
+    for id in 0..shared.executor_threads() {
+        debug!("Starting executor {}", id);
+        executors.push(std::thread::spawn(enclose! { (shared) move || {
+            tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap()
+                .block_on(async {
+                    server::run_on_new_localset(shared).await
+                }).unwrap();
+        }}));
+    }
+
+    for ex in executors.drain(..) {
+        ex.join().unwrap();
+    }
+
+    if let DoRepeat::Yes = tasks.join().await? {
         info!("Restarting");
         execv(&CString::new(exe).unwrap(), &args).unwrap();
     }
