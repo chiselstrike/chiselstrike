5f673192978aacf36e7abc024037833a564a975a
Dejan Mircevski <dejan@chiselstrike.com>
2021-12-05 22:40:07 -0500

    NextJS example: Use ChiselStrike client for endpts
    Instead of invoking endpoints directly with fetch(), use the client.
    This will let us automatically pass the chiseld session token.
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- examples/nextjs-chisel/lib/chiselstrike.js
+++ examples/nextjs-chisel/lib/chiselstrike.js
@@ -11,7 +11,13 @@ export async function getChiselStrikeClient(session, urlParameters) {
         user: await getUser(token),
         isLoggedIn: !!token,
         loginLink: `https://github.com/login/oauth/authorize?scope=read:user&client_id=${process.env.TAOID}`,
+        backend_url: 'http://localhost:8080/',
     };
     await session.save();
     return session.chiselstrikeClient;
+}
+
+export async function chiselFetch(client, endpoint, opts) {
+    // FIXME: we should use client.backend_url instead of localhost:3000 here, but that currently fails CORS preflight.
+    return await fetch(endpoint, opts)
 }
