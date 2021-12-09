002a31a9d3e5bc101e318dfcedf7bf0e76b5e5d9
Glauber Costa <glauber@chiselstrike.com>
2021-12-09 13:06:55 -0500

    Add a second, local-only web server
    This will be used to support our K8s deployment. Liveness, readiness and
    status checks are implemented (in their most simple forms for now)
    
    To avoid having to worry about those being a source of DDoS, this is a
    separate server that listens to 127.0.0.1 only. That's enough for k8s.

---
+++ server/src/internal.rs
@@ -0,0 +1,59 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::path::Path;
+
+async fn route(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    let uri = Path::new(req.uri().path());
+    let component = match uri.file_name() {
+        None => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+            return Ok(response);
+        }
+        Some(x) => x,
+    };
+    match component.to_str() {
+        None => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+        // Conceptually those checks are different and could eventually become
+        // more complex functions. But for now we just return simple strings.
+        // FWIW, K8s does not require us to return those specific strings.
+        // Anything that returns a code 200 is enough.
+        Some(x) => match x {
+            "status" => {
+                *response.body_mut() = "ok".into();
+            }
+            "readiness" => {
+                *response.body_mut() = "ready".into();
+            }
+            "liveness" => {
+                *response.body_mut() = "alive".into();
+            }
+            _ => {
+                *response.status_mut() = StatusCode::NOT_FOUND;
+            }
+        },
+    }
+    Ok(response)
+}
+
+/// Initialize ChiselStrike's internal routes.
+///
+/// Unlike the API server, it is strictly bound to 127.0.0.1. This is enough
+/// for the Kubernetes checks to work, and it is one less thing for us to secure
+/// and prevent DDoS attacks again - which is why this is a different server
+pub(crate) fn init(addr: SocketAddr) {
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(route))
+    });
+
+    tokio::task::spawn(async move {
+        let server = Server::bind(&addr).serve(make_svc);
+        server.await
+    });
+}
