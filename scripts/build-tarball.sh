e19c5ea1bcb5a0f15f62af6f6963fddef504ce77
Pekka Enberg <penberg@chiselstrike.com>
2021-12-14 10:01:51 +0200

    Tarball build script
    This adds a script for building ChiselStrike tarballs for distribution.
    
    Example usage:
    
    % ./scripts/build-tarball.sh -t aarch64-apple-darwin
    
    % tar tf chiselstrike-aarch64-apple-darwin.tar.gz
    chiselstrike-aarch64-apple-darwin/
    chiselstrike-aarch64-apple-darwin/chisel
    chiselstrike-aarch64-apple-darwin/chiseld

---
+++ scripts/build-tarball.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Tarball build script.
+#
+# SPDX-FileCopyrightText: © 2021 ChiselStrike <info@chiselstrike.com>
+#
+# This scripts builds a tarball of ChiselStrike for distribution. You need to
+# specify the target (i.e. operating system and machine architecture) you are
+# building for with the `-t TARGET` command line option. Please see `rustc
+# --print target-list` for a list of available targets.
+
+program="chiselstrike"
+
+files=(
+  chisel
+  chiseld
+)
+
+while getopts "t:" option
+do
+  case $option in
+    t)
+      target="$OPTARG"
+      ;;
+  esac
+done
+
+rustup target add "$target"
+cargo build --release --target "$target"
+mkdir -p "builds/$program-$target"
+for file in ${files[@]}
+do
+  cp "target/$target/release/$file" "builds/$program-$target"
+done
+tar -C builds -czvf "$program-$target.tar.gz" "$program-$target/"
