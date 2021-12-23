7e65365058edbf20c4033f717822efd7952060d7
Dejan Mircevski <dejan@chiselstrike.com>
2021-12-23 11:11:33 -0500

    Update NextJS example for latest chiseld
    After 5c369eb, we need a Chisel.toml file and a "policies" directory.
    
    After c7277df, we skip ".collections" after "Chisel" in endpoint code.
    
    After 71df3ed, we need to specify the endpoint version in the URL.
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- examples/nextjs-chisel/next.config.js
+++ examples/nextjs-chisel/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         return [
           {
             source: '/api/:path*',
-            destination: 'http://127.0.0.1:8080/api/:path*',
+            destination: 'http://127.0.0.1:8080/dev/api/:path*',
           },
         ]
       },
