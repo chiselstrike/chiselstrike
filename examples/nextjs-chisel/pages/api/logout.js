112c4e61c136352ffa84f17cb0f83b2be22ddeb8
Dejan Mircevski <dejan@chiselstrike.com>
2021-12-24 11:28:09 -0500

    Drop the NextJS example
    It's been moved to https://github.com/chiselstrike/chiselstrike-examples
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- examples/nextjs-chisel/pages/api/logout.js
+++
@@ -1,8 +0,0 @@
-import { withSessionRoute } from "../../lib/withSession";
-
-export default withSessionRoute(logoutRoute);
-
-async function logoutRoute(req, res) {
-  await req.session.destroy();
-  res.send("Logged out");
-}
