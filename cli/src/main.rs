45c2ff0b0ef0579c8451ba106e88f612800245dc
Glauber Costa <glauber@chiselstrike.com>
2021-12-30 17:29:31 -0500

    fix dts definitions
    There may be a way to auto-generate those from the api file, but until
    we figure out will have to keep it manually synced.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -396,10 +396,11 @@ async fn apply<S: ToString>(
 
 declare type ChiselIterator<T> = {{
     findMany(restrictions: Partial<T>): ChiselIterator<T>;
-    findOne(restrictions: Partial<T>): T | null;
+    findOne(restrictions: Partial<T>): Promise<T | null>;
     select(...columns: (keyof T)[]): ChiselIterator<T>;
     [Symbol.asyncIterator]: () => AsyncIterator<T>;
     join<U>(right: ChiselIterator<U>): ChiselIterator<T & U>;
+    take(limit_: number): ChiselIterator<T>;
 }}
 
 declare type Chisel = {{
