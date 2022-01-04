91acbc35ad32b1760989e1b23e3b1b21aa6be025
Dejan Mircevski <dejan@chiselstrike.com>
2022-01-04 18:59:12 -0500

    internal: Add the SERVE_WEBUI flag
    In the future we'll serve the web UI when this is true.
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- server/src/internal.rs
+++ server/src/internal.rs
@@ -2,20 +2,24 @@
 
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server, StatusCode};
+use once_cell::sync::OnceCell;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+
+/// If set, serve the web UI using this address for gRPC calls.
+static SERVE_WEBUI: OnceCell<SocketAddr> = OnceCell::new();
 
 async fn route(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
 
-    match req.uri().path() {
+    match (req.uri().path(), SERVE_WEBUI.get()) {
         // Conceptually those checks are different and could eventually become
         // more complex functions. But for now we just return simple strings.
         // FWIW, K8s does not require us to return those specific strings.
         // Anything that returns a code 200 is enough.
-        "/status" => *response.body_mut() = "ok".into(),
-        "/readiness" => *response.body_mut() = "ready".into(),
-        "/liveness" => *response.body_mut() = "alive".into(),
+        ("/status", _) => *response.body_mut() = "ok".into(),
+        ("/readiness", _) => *response.body_mut() = "ready".into(),
+        ("/liveness", _) => *response.body_mut() = "alive".into(),
         _ => *response.status_mut() = StatusCode::NOT_FOUND,
     }
     Ok(response)
@@ -26,7 +30,12 @@ async fn route(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 /// Unlike the API server, it is strictly bound to 127.0.0.1. This is enough
 /// for the Kubernetes checks to work, and it is one less thing for us to secure
 /// and prevent DDoS attacks again - which is why this is a different server
-pub(crate) fn init(addr: SocketAddr) {
+pub(crate) fn init(addr: SocketAddr, serve_webui: bool, rpc_addr: SocketAddr) {
+    if serve_webui {
+        SERVE_WEBUI
+            .set(rpc_addr)
+            .expect("SERVE_WEBUI already initialized before internal::init()");
+    }
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
         Ok::<_, Infallible>(service_fn(route))
