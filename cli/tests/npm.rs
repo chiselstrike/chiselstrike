51d4c7bf38ed59cae42aa8a2d9f5419ea47be480
Pekka Enberg <penberg@chiselstrike.com>
2022-01-07 10:00:05 +0200

    tests: Run "npm run build"
    Run the npm package build as part of "cargo test" to make sure api.ts
    builds with tsc.

---
+++ cli/tests/npm.rs
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: © 2022 ChiselStrike <info@chiselstrike.com>
+
+mod common;
+
+#[cfg(test)]
+mod tests {
+    use crate::common::run_in;
+    use std::path::Path;
+
+    #[test]
+    fn npm_build() {
+        run_in("npm", ["run", "build"], Path::new("../npm").to_path_buf());
+    }
+}
