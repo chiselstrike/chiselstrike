cac30d46242e9f97077d713a46b3d12addc08981
Glauber Costa <glauber@chiselstrike.com>
2022-01-08 14:39:26 -0500

    import Chisel in the generated file
    This will make vscode very happy

--- cli/src/template/hello.ts
+++ cli/src/template/hello.ts
@@ -4,6 +4,8 @@
 //
 // curl -d '{"hello": "world"}' localhost:8080/dev/hello
 
+import { Chisel } from "@chiselstrike/chiselstrike";
+
 export default async function (req: Request): Promise<Response> {
     const json = await req.json();
     return Chisel.json(json);
