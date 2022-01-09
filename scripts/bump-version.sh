a9f687713f1f5412426702b935c810a3602cac30
Pekka Enberg <penberg@chiselstrike.com>
2022-01-09 11:17:42 +0200

    Add a script for bumping project version
    We currently need to define project version in multiple metadata files
    (both Rust crate and npm package) so let's add a helper script to do
    that.

---
+++ scripts/bump-version.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# SPDX-FileCopyrightText: © 2022 ChiselStrike <info@chiselstrike.com>
+#
+# Bump ChiselStrike project version across all the metadata files.
+#
+# Example usage:
+#
+#  bump-version.sh 0.1.2
+#
+# Updates the Cargo and npm metadata files with the version "0.1.2", makes a
+# git commit, and creates a "v0.1.2" git tag.
+
+if [ $# -eq 0 ]; then
+  cat << EOF
+usage: bump-version.sh [version]
+
+Example usage:
+
+  bump-version.sh 0.1.2
+EOF
+    exit 1
+fi
+
+version=$1
+
+cargo set-version --workspace $1
+
+cd npm && npm version --no-git-tag-version $version
+
+git commit -a -m "ChiselStrike v$version"
+
+git tag "v$version"
