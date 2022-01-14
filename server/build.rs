3fc40830c9f496f7934c5ee59d848923459ced31
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-01-14 16:10:28 -0500

    Simplify server/build.rs

--- server/build.rs
+++ server/build.rs
@@ -3,22 +3,12 @@
 use anyhow::Result;
 use vergen::{vergen, Config, SemverKind};
 
-fn build_proto() -> Result<()> {
+fn main() -> Result<()> {
     let proto = "../proto/chisel.proto";
     tonic_build::compile_protos(proto)?;
     println!("cargo:rerun-if-changed={}", proto);
-    Ok(())
-}
-
-fn build_version() -> Result<()> {
     let mut config = Config::default();
     *config.git_mut().semver_kind_mut() = SemverKind::Lightweight;
     vergen(config)?;
-    Ok(())
-}
-
-fn main() -> Result<()> {
-    build_proto()?;
-    build_version()?;
     Ok(())
 }
