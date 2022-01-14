0f2bbc76797d93f1f967534d2c7d12e0cec1fdff
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-01-14 11:14:23 -0500

    Compile chisel.ts in build.rs
    This removes one use of a typescript compiler in chiseld and takes the
    test time from 183.43s to 60.81s.

--- server/build.rs
+++ server/build.rs
@@ -1,7 +1,31 @@
 // SPDX-FileCopyrightText: © 2021 ChiselStrike <info@chiselstrike.com>
 
 use anyhow::Result;
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use tsc_compile::compile_ts_code;
 use vergen::{vergen, Config, SemverKind};
+
+fn build_chisel() -> Result<()> {
+    let chisel_ts = "src/chisel.ts";
+
+    // Every other file we use comes from the snapshot, so these
+    // should be the only rerun-if-changed that we need.
+    println!("cargo:rerun-if-changed={}", chisel_ts);
+    println!("cargo:rerun-if-changed=src/dts/lib.deno_core.d.ts");
+
+    let mut map = compile_ts_code(chisel_ts, None)?;
+    let code = map.remove(chisel_ts).unwrap();
+
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    fs::write(&out.join("chisel.js"), code)?;
+    fs::write(
+        &out.join("chisel.d.ts"),
+        map.remove("src/chisel.d.ts").unwrap(),
+    )?;
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let proto = "../proto/chisel.proto";
@@ -10,5 +34,6 @@ fn main() -> Result<()> {
     let mut config = Config::default();
     *config.git_mut().semver_kind_mut() = SemverKind::Lightweight;
     vergen(config)?;
+    build_chisel()?;
     Ok(())
 }
