6f45c05390f9fc3ee10759b4f9919624fdb8a170
Pekka Enberg <penberg@chiselstrike.com>
2022-01-14 15:47:01 +0200

    Add 'chiselstrike-cli' package
    This adds a 'chiselstrike-cli' package, which includes just the
    ChiselStrike binaries. The 'chiselstrike' package is now just the
    ChiselStrike API.
    
    The upside is that now CLI packaging is easier to reason about, which
    makes it easier to fix a packaging bug where "getBinary.js" was left ouf
    the package, causing the following issue when attempting to run "chisel
    dev" from package.json scripts:
    
    % npm run dev
    
    > hello@1.0.0 dev
    > chisel dev
    
    node:internal/modules/cjs/loader:936
      throw err;
      ^
    
    Error: Cannot find module './getBinary'
    Require stack:
    - /Users/penberg/src/chiselstrike/chiselstrike/packages/create-chisel-app/foo/node_modules/@chiselstrike/chiselstrike/npm/run.js
        at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)
        at Function.Module._load (node:internal/modules/cjs/loader:778:27)
        at Module.require (node:internal/modules/cjs/loader:999:19)
        at require (node:internal/modules/cjs/helpers:102:18)
        at Object.<anonymous> (/Users/penberg/src/chiselstrike/chiselstrike/packages/create-chisel-app/foo/node_modules/@chiselstrike/chiselstrike/npm/run.js:3:19)
        at Module._compile (node:internal/modules/cjs/loader:1097:14)
        at Object.Module._extensions..js (node:internal/modules/cjs/loader:1149:10)
        at Module.load (node:internal/modules/cjs/loader:975:32)
        at Function.Module._load (node:internal/modules/cjs/loader:822:12)
        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12) {
      code: 'MODULE_NOT_FOUND',
      requireStack: [
        '/Users/penberg/src/chiselstrike/chiselstrike/packages/create-chisel-app/foo/node_modules/@chiselstrike/chiselstrike/npm/run.js'
      ]
    }

---
+++ packages/chiselstrike-cli/binary.js
@@ -0,0 +1,38 @@
+const { Binary } = require("binary-install");
+
+const os = require("os");
+
+const { version, name, repository } = require("./package.json");
+
+const getTarget = () => {
+  const type = os.type();
+  const arch = os.arch();
+
+  if (type === "Linux" && arch == "x64") return "x86_64-unknown-linux-gnu";
+  if (type === "Darwin" && arch === "x64") return "x86_64-apple-darwin";
+  if (type === "Darwin" && arch === "arm64") return "aarch64-apple-darwin";
+
+  throw new Error(`Unsupported platform: ${type} ${arch}`);
+}
+
+const getBinary = () => {
+  const target = getTarget();
+  const url = `https://downloads.chiselstrike.com/chiselstrike/beta/chiselstrike-v${version}-${target}.tar.gz`;
+  const name = 'chisel';
+  return new Binary(name, url);
+}
+
+const install = () => {
+  const binary = getBinary();
+  binary.install();
+};
+
+const run = () => {
+  const binary = getBinary();
+  binary.run();
+};
+
+module.exports = {
+  install,
+  run
+};
