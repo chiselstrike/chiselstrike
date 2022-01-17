9843f789fbffb71d11cb2a15daec7a221152c5e9
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-01-17 13:35:19 -0500

    Don't lint lib.deno_core.d.ts
    This was getting noisy and distracting from warnings in our code.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
     ],
-    ignorePatterns: ["/examples/", "/cli/examples/", "third_party/", "template/"],
+    ignorePatterns: ["/examples/", "/cli/examples/", "third_party/", "template/", "packages/chiselstrike/src/lib.deno_core.d.ts",
+                     "packages/chiselstrike/lib/lib.deno_core.d.ts", "api/src/lib.deno_core.d.ts"],
     rules: {
         "@typescript-eslint/switch-exhaustiveness-check": "error",
         "@typescript-eslint/triple-slash-reference": "off",
