b53694374972794add4586061e0e4d99d37b64ed
Pekka Enberg <penberg@chiselstrike.com>
2022-01-19 09:48:58 +0200

    Fix json import in project template

--- packages/create-chiselstrike-app/template/hello.ts
+++ packages/create-chiselstrike-app/template/hello.ts
@@ -4,9 +4,9 @@
 //
 // curl -d '{"hello": "world"}' localhost:8080/dev/hello
 
-import { Chisel } from "@chiselstrike/chiselstrike";
+import { json } from "@chiselstrike/chiselstrike"
 
 export default async function (req: Request): Promise<Response> {
     const json = await req.json();
-    return Chisel.json(json);
+    return json(json);
 }
