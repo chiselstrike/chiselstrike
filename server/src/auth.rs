079575ce15af4b1c8c2157f5da977e0be63a85b6
Dejan Mircevski <dejan@chiselstrike.com>
2022-01-19 23:36:18 -0500

    Factor out auth::get_username()
    We will reuse it in run_js to provide op_chisel_user.
    
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- server/src/auth.rs
+++ server/src/auth.rs
@@ -101,3 +101,14 @@ pub(crate) fn init(api: &mut ApiService) {
     );
     api.add_route(USERPATH.into(), Arc::new(lookup_user));
 }
+
+pub(crate) async fn get_username(req: &Request<hyper::Body>) -> anyhow::Result<Option<String>> {
+    match req.headers().get("ChiselStrikeToken") {
+        Some(token) => {
+            let token = token.to_str()?;
+            let meta = { crate::runtime::get().meta.clone() };
+            Ok(meta.get_username(token).await.ok())
+        }
+        None => Ok(None),
+    }
+}
