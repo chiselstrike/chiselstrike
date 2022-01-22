e4bee2b7b1c8914109cf9e715ca87d841bd707fe
Glauber Costa <glauber@chiselstrike.com>
2022-01-22 16:39:47 -0500

    run linters in our packages as well
    I ran the linter in one of our files while developing and changes were
    massive. Turns out we're not running it at all, so as a preparation
    patch I am sending this separately. Otherwise nobody will be able
    to read my PRs!

--- packages/chiselstrike-frontend/index.mjs
+++ packages/chiselstrike-frontend/index.mjs
@@ -2,18 +2,23 @@
 
 async function getUser(token) {
     if (!token) return null;
-    const resp = await fetch(`http://localhost:8080/__chiselstrike/auth/user/${token}`);
+    const resp = await fetch(
+        `http://localhost:8080/__chiselstrike/auth/user/${token}`,
+    );
     return resp.ok ? await resp.text() : "failure: " + resp.status;
 }
 
 export async function getChiselStrikeClient(session, urlParameters) {
-    if (session.chiselstrikeClient && session.chiselstrikeClient.token) return session.chiselstrikeClient;
+    if (session.chiselstrikeClient && session.chiselstrikeClient.token) {
+        return session.chiselstrikeClient;
+    }
     const token = urlParameters.chiselstrike_token ?? null;
     session.chiselstrikeClient = {
         user: await getUser(token),
         token,
-        loginLink: `https://github.com/login/oauth/authorize?scope=read:user&client_id=2d4672c296ae275cd320`,
-        backend_url: 'http://localhost:8080/',
+        loginLink:
+            `https://github.com/login/oauth/authorize?scope=read:user&client_id=2d4672c296ae275cd320`,
+        backend_url: "http://localhost:8080/",
     };
     await session.save();
     return session.chiselstrikeClient;
@@ -21,8 +26,8 @@ export async function getChiselStrikeClient(session, urlParameters) {
 
 export async function chiselFetch(client, endpoint, opts) {
     if (client.token) {
-        if (!opts.headers) opts.headers = {}
+        if (!opts.headers) opts.headers = {};
         opts.headers.ChiselStrikeToken = client.token;
     }
-    return await fetch(client.backend_url + endpoint, opts)
+    return await fetch(client.backend_url + endpoint, opts);
 }
