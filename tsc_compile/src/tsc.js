chiselstrike/chiselstrike

commit b980e0284406019e3e460571a7bd2226165f6fd5

improve error message in case modules can't be resolved.
Right now if we fail to compile code, we call unwrap(). That's not
ideal, as we'd like to return more gracefully. However, that's not
enough since even that, will just return a cryptic error message.

To improve the situation, in places that we know are likely to fail
due to user error (like module resolution, where the user can just
refer to an non existing file), we'll do better by capturing the error
into diagnostics and adding helpful context.

Before:

```
deno:core/01_core.js:4083: Uncaught Error: No such file or directory (os error 2)
thread 'main' panicked at 'called `Option::unwrap()` on a `None` value', tsc_compile/src/lib.rs:240:10
note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
```

After:

```
Error: parsing endpoint /dev/writer

Caused by:
    Compilation failed:
    Resolving ../models/models.js from /Users/glauber/pekka3/endpoints/writer.ts

    Caused by:
        No such file or directory (os error 2)
```

tsc_compile/src/tsc.js
@@ -128,22 +128,26 @@ function compile(file, lib) {
         return v;
     };
 
-    const program = ts.createProgram([file], options, host);
-    const emitResult = program.emit();
+    try {
+        const program = ts.createProgram([file], options, host);
+        const emitResult = program.emit();
 
-    let allDiagnostics = ts
-        .getPreEmitDiagnostics(program)
-        .concat(emitResult.diagnostics);
+        let allDiagnostics = ts
+            .getPreEmitDiagnostics(program)
+            .concat(emitResult.diagnostics);
 
-    allDiagnostics = ts.sortAndDeduplicateDiagnostics(allDiagnostics);
-    if (allDiagnostics.length != 0) {
-        const diag = ts.formatDiagnosticsWithColorAndContext(
-            allDiagnostics,
-            host,
-        );
-        Deno.core.opSync("diagnostic", diag);
+        allDiagnostics = ts.sortAndDeduplicateDiagnostics(allDiagnostics);
+        if (allDiagnostics.length != 0) {
+            const diag = ts.formatDiagnosticsWithColorAndContext(
+                allDiagnostics,
+                host,
+            );
+            Deno.core.opSync("diagnostic", diag);
+        }
+        return !emitResult.emitSkipped;
+    } catch (e) {
+        undefined;
     }
-    return !emitResult.emitSkipped;
 }
 
 compile("bootstrap.ts", undefined);

tsc_compile/src/lib.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: © 2022 ChiselStrike <info@chiselstrike.com>
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use deno_core::op_sync;
 use deno_core::JsRuntime;
 use deno_core::OpState;
@@ -84,7 +84,15 @@ fn fetch_aux(map: &mut DownloadMap, path: String, mut base: String) -> Result<St
 }
 
 fn fetch(_op_state: &mut OpState, path: String, base: String) -> Result<String> {
-    FILES.with(|m| fetch_aux(&mut m.borrow_mut(), path, base))
+    FILES.with(|m| {
+        let mut map = m.borrow_mut();
+        fetch_aux(&mut map, path.clone(), base.clone())
+            .with_context(|| format!("Resolving {} from {}", path, base))
+            .map_err(|x| {
+                map.diagnostics += &format!("{:?}", x);
+                x
+            })
+    })
 }
 
 fn read_aux(map: &mut DownloadMap, path: String) -> Result<String> {
@@ -98,7 +106,15 @@ fn read_aux(map: &mut DownloadMap, path: String) -> Result<String> {
 }
 
 fn read(_op_state: &mut OpState, path: String, _: ()) -> Result<String> {
-    FILES.with(|m| read_aux(&mut m.borrow_mut(), path))
+    FILES.with(|m| {
+        let mut map = m.borrow_mut();
+        read_aux(&mut map, path.clone())
+            .with_context(|| format!("Reading {}", path))
+            .map_err(|x| {
+                map.diagnostics += &format!("{:?}", x);
+                x
+            })
+    })
 }
 
 fn write_aux(map: &mut DownloadMap, mut path: String, content: String) {
@@ -229,8 +245,7 @@ pub fn compile_ts_code(
 
     if !compile
         .call(scope, global_proxy.into(), &[file, lib])
-        .unwrap()
-        .is_true()
+        .map_or(false, |x| x.is_true())
     {
         return Err(FILES.with(|m| anyhow!("Compilation failed:\n{}", m.borrow().diagnostics)));
     }

commit 1cf64507ba05a830a63eef2ffbd97393294ccf38

Don't eat an exception
This was introduced in b980e0284406019e3e460571a7bd2226165f6fd5, but I
don't see how ignoring an exception helps now that we don't unwrap()
the call result.

tsc_compile/src/tsc.js
@@ -128,26 +128,22 @@ function compile(file, lib) {
         return v;
     };
 
-    try {
-        const program = ts.createProgram([file], options, host);
-        const emitResult = program.emit();
+    const program = ts.createProgram([file], options, host);
+    const emitResult = program.emit();
 
-        let allDiagnostics = ts
-            .getPreEmitDiagnostics(program)
-            .concat(emitResult.diagnostics);
+    let allDiagnostics = ts
+        .getPreEmitDiagnostics(program)
+        .concat(emitResult.diagnostics);
 
-        allDiagnostics = ts.sortAndDeduplicateDiagnostics(allDiagnostics);
-        if (allDiagnostics.length != 0) {
-            const diag = ts.formatDiagnosticsWithColorAndContext(
-                allDiagnostics,
-                host,
-            );
-            Deno.core.opSync("diagnostic", diag);
-        }
-        return !emitResult.emitSkipped;
-    } catch (e) {
-        undefined;
+    allDiagnostics = ts.sortAndDeduplicateDiagnostics(allDiagnostics);
+    if (allDiagnostics.length != 0) {
+        const diag = ts.formatDiagnosticsWithColorAndContext(
+            allDiagnostics,
+            host,
+        );
+        Deno.core.opSync("diagnostic", diag);
     }
+    return !emitResult.emitSkipped;
 }
 
 compile("bootstrap.ts", undefined);