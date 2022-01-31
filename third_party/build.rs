635178d783ff275b3a592cfdabdf3ec94c9c8216
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-01-31 12:26:43 -0500

    Check the submodule revision

--- third_party/build.rs
+++ third_party/build.rs
@@ -5,4 +5,15 @@ fn main() {
         .args(["submodule", "update", "--init"])
         .status()
         .unwrap();
+
+    let out = Command::new("git")
+        .args(["submodule", "status"])
+        .output()
+        .unwrap()
+        .stdout;
+
+    // Check that we got the correct revision. The main reason for
+    // this is to force build.rs to change and the build to be rerun
+    // when updating the submodule.
+    assert!(out.starts_with(b" df5fe5a35f8ace40275a822927f3385c828beba7 deno"));
 }
