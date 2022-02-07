a5347ed89e936468c72a6adee46d5ebf138142b5
Pekka Enberg <penberg@chiselstrike.com>
2022-02-07 09:25:50 +0200

    cli: Move project related code to project.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: © 2021 ChiselStrike <info@chiselstrike.com>
 
 use crate::chisel::StatusResponse;
+use crate::project::{create_project, read_manifest, read_to_string, CreateProjectOptions, Module};
 use anyhow::{anyhow, Context, Result};
 use chisel::chisel_rpc_client::ChiselRpcClient;
 use chisel::{
@@ -11,14 +12,12 @@ use compile::compile_ts_code as swc_compile;
 use futures::channel::mpsc::channel;
 use futures::{SinkExt, StreamExt};
 use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
-use serde_derive::Deserialize;
-use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::future::Future;
-use std::io::{stdin, ErrorKind, Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::{ErrorKind, Write};
+use std::path::Path;
 use std::thread;
 use std::time::Duration;
 use structopt::StructOpt;
@@ -29,43 +28,11 @@ use tonic::transport::Channel;
 use tsc_compile::compile_ts_code;
 use tsc_compile::CompileOptions;
 
+mod project;
 mod ts;
 
 // Timeout when waiting for connection or server status.
 const TIMEOUT: Duration = Duration::from_secs(10);
-
-#[derive(Deserialize, PartialEq)]
-enum Module {
-    #[serde(rename = "node")]
-    Node,
-    #[serde(rename = "deno")]
-    Deno,
-}
-
-impl Default for Module {
-    fn default() -> Self {
-        Module::Node
-    }
-}
-
-/// Manifest defines the files that describe types, endpoints, and policies.
-///
-/// The manifest is a high-level declaration of application behavior.
-/// The individual definitions are passed to `chiseld`, which processes them
-/// accordingly. For example, type definitions are imported as types and
-/// endpoints are made executable via Deno.
-#[derive(Deserialize)]
-struct Manifest {
-    /// Vector of directories to scan for model definitions.
-    models: Vec<String>,
-    /// Vector of directories to scan for endpoint definitions.
-    endpoints: Vec<String>,
-    /// Vector of directories to scan for policy definitions.
-    policies: Vec<String>,
-    /// Whether to use deno-style or node-style modules
-    #[serde(default)]
-    modules: Module,
-}
 
 enum AllowTypeDeletion {
     No,
@@ -114,94 +81,9 @@ impl From<bool> for TypeChecking {
     }
 }
 
-fn dir_to_paths(dir: &Path, paths: &mut Vec<PathBuf>) -> anyhow::Result<()> {
-    for dentry in read_dir(dir)? {
-        let dentry = dentry?;
-        let path = dentry.path();
-        if dentry.file_type()?.is_dir() {
-            dir_to_paths(&path, paths)?;
-        } else if !dentry.file_name().to_str().map_or(false, ignore_path) {
-            // files with names that can't be converted wtih to_str() or that start with . are
-            // ignored
-            paths.push(path);
-        }
-    }
-    Ok(())
-}
-
-fn ignore_path(path: &str) -> bool {
-    if path.starts_with('.') {
-        return true;
-    }
-    if path.ends_with('~') {
-        return true;
-    }
-    if path.starts_with('#') && path.ends_with('#') {
-        // Emacs auto-save files.
-        return true;
-    }
-    false
-}
-
 fn parse_version(version: &str) -> anyhow::Result<String> {
     anyhow::ensure!(!version.is_empty(), "version name can't be empty");
     Ok(version.to_string())
-}
-
-#[derive(PartialOrd, PartialEq, Eq, Ord)]
-struct Endpoint {
-    name: String,
-    file_path: PathBuf,
-}
-
-impl Manifest {
-    pub fn models(&self) -> anyhow::Result<Vec<PathBuf>> {
-        Self::dirs_to_paths(&self.models)
-    }
-
-    pub fn endpoints(&self) -> anyhow::Result<Vec<Endpoint>> {
-        let mut ret = vec![];
-        for dir in &self.endpoints {
-            let mut paths = vec![];
-            let dir = Path::new(dir);
-            dir_to_paths(dir, &mut paths)?;
-            let mut routes = BTreeMap::new();
-            for file_path in paths {
-                // file_stem returns None only if there is no file name.
-                let stem = file_path.file_stem().unwrap();
-                // parent returns None only for the root.
-                let mut parent = file_path.parent().unwrap().to_path_buf();
-                parent.push(stem);
-
-                let name = parent.strip_prefix(&dir)?;
-
-                if let Some(old) = routes.insert(name.to_owned(), file_path.to_owned()) {
-                    anyhow::bail!("Cannot add both {} {} as routes. ChiselStrike uses filesystem-based routing, so we don't know what to do. Sorry! 🥺", old.display(), file_path.display());
-                }
-
-                let name = name
-                    .to_str()
-                    .ok_or_else(|| anyhow!("filename is not utf8 {:?}", name))?
-                    .to_string();
-                ret.push(Endpoint { file_path, name });
-            }
-        }
-        ret.sort_unstable();
-        Ok(ret)
-    }
-
-    pub fn policies(&self) -> anyhow::Result<Vec<PathBuf>> {
-        Self::dirs_to_paths(&self.policies)
-    }
-
-    fn dirs_to_paths(dirs: &[String]) -> anyhow::Result<Vec<PathBuf>> {
-        let mut paths = vec![];
-        for dir in dirs {
-            dir_to_paths(Path::new(dir), &mut paths)?
-        }
-        paths.sort_unstable();
-        Ok(paths)
-    }
 }
 
 static DEFAULT_API_VERSION: &str = "dev";
@@ -278,24 +160,6 @@ pub mod chisel {
     tonic::include_proto!("chisel");
 }
 
-/// Opens and reads an entire file (or stdin, if filename is "-")
-fn read_to_string<P: AsRef<Path>>(filename: P) -> anyhow::Result<String> {
-    if filename.as_ref() == Path::new("-") {
-        let mut s = "".to_string();
-        stdin()
-            .read_to_string(&mut s)
-            .with_context(|| "while reading stdin".to_string())?;
-        Ok(s)
-    } else {
-        fs::read_to_string(filename.as_ref())
-            .with_context(|| format!("while reading {}", filename.as_ref().display()))
-    }
-}
-
-fn read_dir<P: AsRef<Path>>(dir: P) -> anyhow::Result<fs::ReadDir> {
-    fs::read_dir(dir.as_ref()).with_context(|| format!("Could not open {}", dir.as_ref().display()))
-}
-
 // Retry calling 'f(a)' until it succeeds. This uses an exponential
 // backoff and gives up once the timeout has passed. On failure 'f'
 // must return an 'A' that we feed to the next retry.  (This can be
@@ -340,112 +204,6 @@ async fn wait(server_url: String) -> Result<tonic::Response<StatusResponse>> {
         s.map_err(|_| client)
     })
     .await
-}
-
-const MANIFEST_FILE: &str = "Chisel.toml";
-const TYPES_DIR: &str = "./models";
-const ENDPOINTS_DIR: &str = "./endpoints";
-const POLICIES_DIR: &str = "./policies";
-const VSCODE_DIR: &str = "./.vscode/";
-
-/// Writes contents to a file in a directory.
-fn write(contents: &[u8], dir: &Path, file: &str) -> Result<()> {
-    let s = std::str::from_utf8(contents)?.to_string();
-    fs::write(dir.join(file), s).map_err(|e| e.into())
-}
-
-/// Writes "template/$file" content into $dir/$file.  The file content is read at compile time but written at
-/// runtime.
-macro_rules! write_template {
-    ( $file:expr, $dir:expr ) => {{
-        write(include_bytes!(concat!("template/", $file)), $dir, $file)
-    }};
-}
-
-/// Project creation options.
-struct CreateProjectOptions {
-    /// Force project creation by overwriting existing project files.
-    force: bool,
-    /// Generate example code for project.
-    examples: bool,
-}
-
-fn create_project(path: &Path, opts: CreateProjectOptions) -> Result<()> {
-    if !opts.force && project_exists(path) {
-        anyhow::bail!("You cannot run `chisel init` on an existing ChiselStrike project");
-    }
-    fs::create_dir_all(path.join(TYPES_DIR))?;
-    fs::create_dir_all(path.join(ENDPOINTS_DIR))?;
-    fs::create_dir_all(path.join(POLICIES_DIR))?;
-    fs::create_dir_all(path.join(VSCODE_DIR))?;
-    write_template!("package.json", path)?;
-    write_template!("tsconfig.json", path)?;
-    write_template!("Chisel.toml", path)?;
-    // creating through chisel instead of npx: default to deno resolution
-    let mut toml = include_bytes!("template/Chisel.toml").to_vec();
-    toml.extend_from_slice("modules = \"deno\"\n".as_bytes());
-    write(&toml, path, "Chisel.toml")?;
-
-    write_template!("settings.json", &path.join(VSCODE_DIR))?;
-
-    if opts.examples {
-        write_template!("hello.ts", &path.join(ENDPOINTS_DIR))?;
-    }
-    println!("Created ChiselStrike project in {}", path.display());
-    Ok(())
-}
-
-fn project_exists(path: &Path) -> bool {
-    path.join(Path::new(MANIFEST_FILE)).exists()
-        || path.join(Path::new(TYPES_DIR)).exists()
-        || path.join(Path::new(ENDPOINTS_DIR)).exists()
-        || path.join(Path::new(POLICIES_DIR)).exists()
-}
-
-fn read_manifest() -> Result<Manifest> {
-    let cwd = env::current_dir()?;
-    if !Path::new(MANIFEST_FILE).exists() {
-        anyhow::bail!("Could not find `{}` in `{}`. Did you forget to run `chisel init` to initialize the project?", MANIFEST_FILE, cwd.display());
-    }
-    let manifest = read_to_string(MANIFEST_FILE)?;
-    let manifest: Manifest = match toml::from_str(&manifest) {
-        Ok(manifest) => manifest,
-        Err(error) => {
-            anyhow::bail!(
-                "Failed to parse manifest at `{}`:\n\n{}",
-                cwd.join(MANIFEST_FILE).display(),
-                error
-            );
-        }
-    };
-    for dir in &manifest.models {
-        if !Path::new(dir).exists() {
-            anyhow::bail!(
-                "Manifest at `{}` has models directory `{}` that does not exist.",
-                cwd.join(MANIFEST_FILE).display(),
-                cwd.join(dir).display()
-            );
-        }
-    }
-    for dir in &manifest.endpoints {
-        if !Path::new(dir).exists() {
-            anyhow::bail!(
-                "Manifest at `{}` has endpoints directory `{}` that does not exist.",
-                cwd.join(MANIFEST_FILE).display(),
-                cwd.join(dir).display()
-            );
-        }
-    }
-    for dir in &manifest.policies {
-        if !Path::new(dir).exists() {
-            anyhow::bail!(
-                "Manifest at `{}` has policies directory `{}` that does not exist.",
-                cwd.join(MANIFEST_FILE).display(),
-                cwd.join(dir).display()
-            );
-        }
-    }
-    Ok(manifest)
 }
 
 fn start_server() -> anyhow::Result<std::process::Child> {
