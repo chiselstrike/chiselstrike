6c654caba72b78f9572f1dc2f505c200aa44f33d
Pekka Enberg <penberg@chiselstrike.com>
2022-02-07 10:44:21 +0200

    cli: Extract cmd_dev() helper

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -279,45 +279,7 @@ async fn main() -> Result<()> {
             }
         }
         Command::Dev { type_check } => {
-            let type_check = type_check.into();
-            let manifest = read_manifest()?;
-            let mut server = start_server()?;
-            wait(server_url.clone()).await?;
-            apply_from_dev(server_url.clone(), type_check).await;
-            let (mut tx, mut rx) = channel(1);
-            let mut apply_watcher =
-                RecommendedWatcher::new(move |res: Result<Event, notify::Error>| {
-                    futures::executor::block_on(async {
-                        tx.send(res).await.unwrap();
-                    });
-                })?;
-            let watcher_config = notify::Config::OngoingEvents(Some(Duration::from_millis(100)));
-            apply_watcher.configure(watcher_config)?;
-            for models_dir in &manifest.models {
-                let models_dir = Path::new(models_dir);
-                apply_watcher.watch(models_dir, RecursiveMode::Recursive)?;
-            }
-            for endpoints_dir in &manifest.endpoints {
-                let endpoints_dir = Path::new(endpoints_dir);
-                apply_watcher.watch(endpoints_dir, RecursiveMode::Recursive)?;
-            }
-            for policies_dir in &manifest.policies {
-                let policies_dir = Path::new(policies_dir);
-                apply_watcher.watch(policies_dir, RecursiveMode::Recursive)?;
-            }
-            while let Some(res) = rx.next().await {
-                match res {
-                    Ok(notify::event::Event {
-                        kind: notify::event::EventKind::Modify(notify::event::ModifyKind::Data(_)),
-                        ..
-                    }) => {
-                        apply_from_dev(server_url.clone(), type_check).await;
-                    }
-                    Ok(_) => { /* ignore */ }
-                    Err(e) => println!("watch error: {:?}", e),
-                }
-            }
-            server.wait()?;
+            cmd_dev(server_url.clone(), type_check).await?;
         }
         Command::New { path, no_examples } => {
             let path = Path::new(&path);
@@ -388,5 +350,48 @@ async fn main() -> Result<()> {
             populate(server_url, version, from).await?;
         }
     }
+    Ok(())
+}
+
+async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> {
+    let type_check = type_check.into();
+    let manifest = read_manifest()?;
+    let mut server = start_server()?;
+    wait(server_url.clone()).await?;
+    apply_from_dev(server_url.clone(), type_check).await;
+    let (mut tx, mut rx) = channel(1);
+    let mut apply_watcher = RecommendedWatcher::new(move |res: Result<Event, notify::Error>| {
+        futures::executor::block_on(async {
+            tx.send(res).await.unwrap();
+        });
+    })?;
+    let watcher_config = notify::Config::OngoingEvents(Some(Duration::from_millis(100)));
+    apply_watcher.configure(watcher_config)?;
+    for models_dir in &manifest.models {
+        let models_dir = Path::new(models_dir);
+        apply_watcher.watch(models_dir, RecursiveMode::Recursive)?;
+    }
+    for endpoints_dir in &manifest.endpoints {
+        let endpoints_dir = Path::new(endpoints_dir);
+        apply_watcher.watch(endpoints_dir, RecursiveMode::Recursive)?;
+    }
+    for policies_dir in &manifest.policies {
+        let policies_dir = Path::new(policies_dir);
+        apply_watcher.watch(policies_dir, RecursiveMode::Recursive)?;
+    }
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(notify::event::Event {
+                kind: notify::event::EventKind::Modify(notify::event::ModifyKind::Data(_)),
+                ..
+            }) => {
+                apply_from_dev(server_url.clone(), type_check).await;
+            }
+            Ok(_) => { /* ignore */ }
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+    server.wait()?;
+
     Ok(())
 }
