e4bc28f604ef717e0a18f15668d4ca834adcef7e
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-02-11 16:37:09 -0500

    Ignore packages/create-chiselstrike-app/dist in eslint
    It was already ignored in deno.json. As far as I can tell there is a
    race between that file being created and eslint running, causing some
    bot failures.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
     ],
     ignorePatterns: ["/examples/", "/cli/examples/", "third_party/", "template/", "packages/chiselstrike-api/src/lib.deno_core.d.ts",
                      "packages/chiselstrike-api/lib/lib.deno_core.d.ts", "api/src/lib.deno_core.d.ts", "target/",
-                     "packages/chiselstrike-api/lib/chisel.d.ts"],
+                     "packages/chiselstrike-api/lib/chisel.d.ts",
+                     "packages/create-chiselstrike-app/dist"],
     rules: {
         "@typescript-eslint/switch-exhaustiveness-check": "error",
         "@typescript-eslint/triple-slash-reference": "off",
