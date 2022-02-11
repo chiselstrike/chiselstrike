a49f422a793d8abc2b844bf2d6fa12ca487c8778
Pekka Enberg <penberg@chiselstrike.com>
2022-02-11 08:33:06 +0200

    eslintrc: Sort ignore paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,17 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
     ],
-    ignorePatterns: ["/examples/", "/cli/examples/", "third_party/", "template/", "packages/chiselstrike-api/src/lib.deno_core.d.ts",
-                     "packages/chiselstrike-api/lib/lib.deno_core.d.ts", "api/src/lib.deno_core.d.ts", "target/",
-                     "packages/chiselstrike-api/lib/chisel.d.ts"],
+    ignorePatterns: [
+        "/cli/examples/",
+        "/examples/",
+        "api/src/lib.deno_core.d.ts",
+        "packages/chiselstrike-api/lib/chisel.d.ts",
+        "packages/chiselstrike-api/lib/lib.deno_core.d.ts",
+        "packages/chiselstrike-api/src/lib.deno_core.d.ts",
+        "target/",
+        "template/",
+        "third_party/"
+    ],
     rules: {
         "@typescript-eslint/switch-exhaustiveness-check": "error",
         "@typescript-eslint/triple-slash-reference": "off",
