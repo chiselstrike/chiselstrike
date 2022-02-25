f1012dd67f24c4218eceefe5f8998e0811c6c2ea
Dejan Mircevski <dejan@chiselstrike.com>
2022-02-24 21:27:06 -0500

    Add regexparam licence text

--- api/src/chisel.ts
+++ api/src/chisel.ts
@@ -422,8 +422,29 @@ export async function loggedInUser(): Promise<OAuthUser | null> {
 //     import { parse as regExParamParse } from "regexparam";
 // or:
 //     import { parse as regExParamParse } from "regexparam";
-// copied from https://deno.land/x/regexparam@v2.0.0/src/index.js and added TS signature, fixed warnings
-// and minor cleanups
+// In the meantime, the regExParamParse function is copied from
+// https://deno.land/x/regexparam@v2.0.0/src/index.js under MIT License included
+// below. ChiselStrike added the TS signature and minor cleanups.
+//
+// Copyright (c) Luke Edwards <luke.edwards05@gmail.com> (lukeed.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 export function regExParamParse(str: string, loose: boolean) {
     let tmp, pattern = "";
     const keys = [], arr = str.split("/");
