4040e469943c31c4d12019dca4b9a3f1efe815d7
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-02-28 16:07:49 -0100

    Avoid redundant recompiles
    If a build.rs doesn't print anything, cargo assumes that it must be
    rerun when any file in the package changes. This this build.rs runs
    npm, a file always changes.
    
    With this changes we are explicit about when it needs to be rebuilt.

--- packages/build.rs
+++ packages/build.rs
@@ -29,9 +29,27 @@ fn main() {
     let api = Path::new("./chiselstrike-api").to_path_buf();
 
     // build create-chiselstrike-app so we can use it in tests
+    for v in [
+        "create-chiselstrike-app/index.ts",
+        "create-chiselstrike-app/template/Chisel.toml",
+        "create-chiselstrike-app/template/hello.ts",
+        "create-chiselstrike-app/template/package.json",
+        "create-chiselstrike-app/template/settings.json",
+        "create-chiselstrike-app/template/tsconfig.json",
+        "create-chiselstrike-app/tsconfig.json",
+        "create-chiselstrike-app/package.json",
+    ] {
+        println!("cargo:rerun-if-changed=./{}", v);
+    }
     run_in("npm", ["install"], create_app.clone());
     run_in("npm", ["run", "build"], create_app);
 
+    for v in [
+        "chiselstrike-api/tsconfig.json",
+        "chiselstrike-api/package.json",
+    ] {
+        println!("cargo:rerun-if-changed=./{}", v);
+    }
     run_in("npm", ["install"], api.clone());
     run_in("npm", ["run", "build"], api);
 }
