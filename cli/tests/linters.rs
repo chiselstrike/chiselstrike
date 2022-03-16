f13f3fc853d63ddb5d47b9f15edfd9740768985c
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-03-16 12:49:20 -0100

    Add a cargo function

--- cli/tests/linters.rs
+++ cli/tests/linters.rs
@@ -4,22 +4,23 @@ mod common;
 
 #[cfg(test)]
 mod tests {
-    use crate::common::run;
+    use crate::common::{run, Command};
     const USE_NIGHTLY: &str = "+nightly-2022-03-15";
 
+    fn cargo<T: IntoIterator<Item = &'static str>>(args: T) -> Command {
+        run("cargo", args)
+    }
+
     fn cargo_install(version: &'static str, pkg: &'static str, bin: &'static str) {
-        run(
-            "cargo",
-            [
-                "install",
-                "--version",
-                version,
-                pkg,
-                "--bin",
-                bin,
-                "--locked",
-            ],
-        );
+        cargo([
+            "install",
+            "--version",
+            version,
+            pkg,
+            "--bin",
+            bin,
+            "--locked",
+        ]);
     }
 
     #[test]
@@ -30,17 +31,14 @@ mod tests {
 
     #[test]
     fn deno_checks() {
-        run(
-            "cargo",
-            [
-                "install",
-                "--path",
-                "./third_party/deno/cli",
-                "--bin",
-                "deno",
-                "--locked",
-            ],
-        );
+        cargo([
+            "install",
+            "--path",
+            "./third_party/deno/cli",
+            "--bin",
+            "deno",
+            "--locked",
+        ]);
         run("deno", ["lint", "--config", "deno.json"]);
         run("deno", ["fmt", "--config", "deno.json", "--check"]);
     }
@@ -48,30 +46,27 @@ mod tests {
     #[test]
     fn sorted_dependencies() {
         cargo_install("1.0.5", "cargo-sort", "cargo-sort");
-        run("cargo", ["sort", "-w", "-c"]);
+        cargo(["sort", "-w", "-c"]);
     }
 
     #[test]
     fn unused_dependencies() {
         cargo_install("0.1.26", "cargo-udeps", "cargo-udeps");
-        run("cargo", [USE_NIGHTLY, "udeps"]);
+        cargo([USE_NIGHTLY, "udeps"]);
     }
 
     #[test]
     fn check_formating() {
-        run("cargo", ["fmt", "--all", "--", "--check"]);
+        cargo(["fmt", "--all", "--", "--check"]);
     }
 
     #[test]
     fn must_not_suspend() {
-        run(
-            "cargo",
-            [USE_NIGHTLY, "check", "--features", "must_not_suspend"],
-        );
+        cargo([USE_NIGHTLY, "check", "--features", "must_not_suspend"]);
     }
 
     #[test]
     fn check_clippy() {
-        run("cargo", ["clippy", "--all-targets", "--", "-D", "warnings"]);
+        cargo(["clippy", "--all-targets", "--", "-D", "warnings"]);
     }
 }
