3d4e1bd72496ea1ad49647dafcd843989f07324f
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-03-18 10:50:51 -0100

    Go back to installing deno from crates.io
    But find the version by looking at the git submodule. This should
    avoids some rebuilds and reduce the cache size in CI.

--- cli/tests/linters.rs
+++ cli/tests/linters.rs
@@ -4,7 +4,10 @@ mod common;
 
 #[cfg(test)]
 mod tests {
-    use crate::common::{run, Command};
+    use crate::common::{repo_dir, run, Command};
+    use std::fs::File;
+    use std::io::Read;
+    use toml::Value;
 
     fn cargo<'a, T: IntoIterator<Item = &'a str>>(args: T) -> Command {
         run("cargo", args)
@@ -34,17 +37,23 @@ mod tests {
         run("npx", ["eslint", ".", "--ext", ".ts"]);
     }
 
+    fn get_deno_version() -> String {
+        let mut f = File::open(repo_dir().join("third_party/deno/cli/Cargo.toml")).unwrap();
+        let mut s = String::new();
+        f.read_to_string(&mut s).unwrap();
+        let doc = s.parse::<Value>().unwrap();
+        if let Value::String(v) = &doc["package"]["version"] {
+            return v.clone();
+        }
+        panic!("Could not find the deno version");
+    }
+
     #[test]
     fn deno_checks() {
-        cargo([
-            "install",
-            "--path",
-            "./third_party/deno/cli",
-            "--bin",
-            "deno",
-            "--locked",
-        ])
-        .env("CARGO_TARGET_DIR", "./target");
+        // Find our deno version and install that. We don't use --path
+        // because that always reinstall the binary.
+        let version = get_deno_version();
+        cargo_install(&version, "deno", "deno");
         run("deno", ["lint", "--config", "deno.json"]);
         run("deno", ["fmt", "--config", "deno.json", "--check"]);
     }
