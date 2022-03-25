9ea79838ef177dcf6de9b9b5fd836d9848fd8e13
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-03-25 11:04:21 -0100

    Export swc_* from compile
    There should never be a case where we want to use one version of swc
    in compile and another in cli, so reexport.

--- cli/src/ts.rs
+++ cli/src/ts.rs
@@ -1,5 +1,7 @@
 use crate::chisel::{AddTypeRequest, FieldDefinition};
 use anyhow::{anyhow, bail, ensure, Context, Result};
+use compile::swc_common;
+use compile::swc_ecmascript;
 use std::collections::BTreeSet;
 use std::path::Path;
 use swc_common::sync::Lrc;
