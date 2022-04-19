chiselstrike/chiselstrike

commit 67659f06a1a630496e79fd65d7fa0ef790b7dac1

cli: Perform apply only on changed files
Track which files actually got changed when performing apply. This is an
optimization, but also needed for correctness for the next patch. That
patch makes "chisel dev" apply when file is renamed, but that generates
two filesystem events: for the old file and the new file, but we only
want to perform apply for the latter.

cli/src/cmd/apply.rs
@@ -5,9 +5,11 @@ use crate::chisel::{ChiselApplyRequest, EndPointCreationRequest, PolicyUpdateReq
 use crate::project::{read_manifest, read_to_string, Module};
 use anyhow::{anyhow, Context, Result};
 use compile::compile_ts_code as swc_compile;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use std::fs;
 use std::io::Write;
+use std::path::PathBuf;
 use tempfile::Builder;
 use tempfile::NamedTempFile;
 use tokio::task::{spawn_blocking, JoinHandle};
@@ -66,14 +68,31 @@ pub(crate) async fn apply<S: ToString>(
     version: S,
     allow_type_deletion: AllowTypeDeletion,
     type_check: TypeChecking,
+    include_paths: HashSet<PathBuf>,
 ) -> Result<()> {
     let version = version.to_string();
 
     let manifest = read_manifest().with_context(|| "Reading manifest file".to_string())?;
-    let models = manifest.models()?;
-    let endpoints = manifest.endpoints()?;
-    let policies = manifest.policies()?;
+    let mut models = manifest.models()?;
+    let mut endpoints = manifest.endpoints()?;
+    let mut policies = manifest.policies()?;
 
+    if !include_paths.is_empty() {
+        models = models
+            .into_iter()
+            .filter(|path| include_paths.contains(&fs::canonicalize(path).unwrap()))
+            .collect();
+        endpoints = endpoints
+            .into_iter()
+            .filter(|endpoint| {
+                include_paths.contains(&fs::canonicalize(&endpoint.file_path).unwrap())
+            })
+            .collect();
+        policies = policies
+            .into_iter()
+            .filter(|path| include_paths.contains(&fs::canonicalize(path).unwrap()))
+            .collect();
+    }
     let types_req = crate::ts::parse_types(&models)?;
     let mut endpoints_req = vec![];
     let mut policy_req = vec![];

cli/src/cmd/dev.rs
@@ -8,7 +8,9 @@ use anyhow::Result;
 use futures::channel::mpsc::channel;
 use futures::{SinkExt, StreamExt};
 use notify::{event::ModifyKind, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use std::collections::HashSet;
 use std::path::Path;
+use std::path::PathBuf;
 use std::time::Duration;
 
 pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> {
@@ -16,7 +18,7 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
     let manifest = read_manifest()?;
     let mut server = start_server()?;
     wait(server_url.clone()).await?;
-    apply_from_dev(server_url.clone(), type_check).await;
+    apply_from_dev(server_url.clone(), type_check, HashSet::default()).await;
     let (mut tx, mut rx) = channel(1);
     let mut apply_watcher = RecommendedWatcher::new(move |res: Result<Event, notify::Error>| {
         futures::executor::block_on(async {
@@ -41,9 +43,11 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
         match res {
             Ok(Event {
                 kind: EventKind::Modify(ModifyKind::Data(_)),
+                paths,
                 ..
             }) => {
-                apply_from_dev(server_url.clone(), type_check).await;
+                let paths = HashSet::from_iter(paths.into_iter());
+                apply_from_dev(server_url.clone(), type_check, paths).await;
             }
             Ok(_) => { /* ignore */ }
             Err(e) => println!("watch error: {:?}", e),
@@ -54,12 +58,13 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
     Ok(())
 }
 
-async fn apply_from_dev(server_url: String, type_check: TypeChecking) {
+async fn apply_from_dev(server_url: String, type_check: TypeChecking, paths: HashSet<PathBuf>) {
     if let Err(e) = apply(
         server_url,
         DEFAULT_API_VERSION,
         AllowTypeDeletion::No,
         type_check,
+        paths,
     )
     .await
     {

cli/src/main.rs
@@ -9,6 +9,7 @@ use chisel::chisel_rpc_client::ChiselRpcClient;
 use chisel::{
     ChiselDeleteRequest, DescribeRequest, PopulateRequest, RestartRequest, StatusRequest,
 };
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io::ErrorKind;
@@ -252,6 +253,7 @@ async fn main() -> Result<()> {
                 version,
                 allow_type_deletion.into(),
                 type_check.into(),
+                HashSet::default(),
             )
             .await?;
         }

commit e856109022d1dbe711050b0fd56d746f491c3e43

cli: Fix disappearing endpoints with 'chisel dev'
One more fix in the never-ending saga of 'chisel dev' breakage...

The story starts with 'chisel dev' not triggering on file rename
(discussed in #906). To fix that, there was an intermediate commit
67659f06 ("cli: Perform apply only on changed files") that attempted
to address the issue of some editors (hello vim!) generating temporary
files that get renamed on save, which resulted in models being imported
multiple times (for the old and new name with renames).

However, commit 67659f06 pushed the logic of ignoring temporary files at
the wrong layer, resulting in models being first dropped, and then
re-added causing errors (discussed in #1138). That bug got fixed in
commit b3f09430 ("cli: Don't pass temporary files to apply()"), but that
revealed that the optimization was incorrect.

The reason we cannot filter paths in apply() is because none of the
`EndPointCreationRequest` stuff is actually used (it's dead code).
Instead, we always pass a full set of model and endpoint definitions to
the server. So if we filter a path, then the server thinks that path no
longer exists, and removes it.

Fix disappearing endpoints with 'chisel dev' by undoing the broken
optimization in commit 67659f06.

Fixes #1156

cli/src/cmd/apply.rs
@@ -5,11 +5,9 @@ use crate::chisel::{ChiselApplyRequest, EndPointCreationRequest, PolicyUpdateReq
 use crate::project::{read_manifest, read_to_string, Module};
 use anyhow::{anyhow, Context, Result};
 use compile::compile_ts_code as swc_compile;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::env;
-use std::fs;
 use std::io::Write;
-use std::path::PathBuf;
 use tempfile::Builder;
 use tempfile::NamedTempFile;
 use tokio::task::{spawn_blocking, JoinHandle};
@@ -68,31 +66,14 @@ pub(crate) async fn apply<S: ToString>(
     version: S,
     allow_type_deletion: AllowTypeDeletion,
     type_check: TypeChecking,
-    include_paths: HashSet<PathBuf>,
 ) -> Result<()> {
     let version = version.to_string();
 
     let manifest = read_manifest().with_context(|| "Reading manifest file".to_string())?;
-    let mut models = manifest.models()?;
-    let mut endpoints = manifest.endpoints()?;
-    let mut policies = manifest.policies()?;
+    let models = manifest.models()?;
+    let endpoints = manifest.endpoints()?;
+    let policies = manifest.policies()?;
 
-    if !include_paths.is_empty() {
-        models = models
-            .into_iter()
-            .filter(|path| include_paths.contains(&fs::canonicalize(path).unwrap()))
-            .collect();
-        endpoints = endpoints
-            .into_iter()
-            .filter(|endpoint| {
-                include_paths.contains(&fs::canonicalize(&endpoint.file_path).unwrap())
-            })
-            .collect();
-        policies = policies
-            .into_iter()
-            .filter(|path| include_paths.contains(&fs::canonicalize(path).unwrap()))
-            .collect();
-    }
     let types_req = crate::ts::parse_types(&models)?;
     let mut endpoints_req = vec![];
     let mut policy_req = vec![];

cli/src/cmd/dev.rs
@@ -18,7 +18,7 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
     let manifest = read_manifest()?;
     let mut server = start_server()?;
     wait(server_url.clone()).await?;
-    apply_from_dev(server_url.clone(), type_check, HashSet::default()).await;
+    apply_from_dev(server_url.clone(), type_check).await;
     let (mut tx, mut rx) = channel(1);
     let mut apply_watcher = RecommendedWatcher::new(move |res: Result<Event, notify::Error>| {
         futures::executor::block_on(async {
@@ -51,9 +51,9 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
                         .into_iter()
                         .filter(|path| !crate::project::ignore_path(path.to_str().unwrap())),
                 );
-                let paths = HashSet::from_iter(paths.into_iter());
+                let paths: HashSet<PathBuf> = HashSet::from_iter(paths.into_iter());
                 if !paths.is_empty() {
-                    apply_from_dev(server_url.clone(), type_check, paths).await;
+                    apply_from_dev(server_url.clone(), type_check).await;
                 }
             }
             Ok(_) => { /* ignore */ }
@@ -65,13 +65,12 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
     Ok(())
 }
 
-async fn apply_from_dev(server_url: String, type_check: TypeChecking, paths: HashSet<PathBuf>) {
+async fn apply_from_dev(server_url: String, type_check: TypeChecking) {
     if let Err(e) = apply(
         server_url,
         DEFAULT_API_VERSION,
         AllowTypeDeletion::No,
         type_check,
-        paths,
     )
     .await
     {

cli/src/main.rs
@@ -9,7 +9,6 @@ use chisel::chisel_rpc_client::ChiselRpcClient;
 use chisel::{
     ChiselDeleteRequest, DescribeRequest, PopulateRequest, RestartRequest, StatusRequest,
 };
-use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io::ErrorKind;
@@ -253,7 +252,6 @@ async fn main() -> Result<()> {
                 version,
                 allow_type_deletion.into(),
                 type_check.into(),
-                HashSet::default(),
             )
             .await?;
         }