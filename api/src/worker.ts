ce11a4ffd4cf6004dd4b923a33905070cfbef661
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-04-21 16:04:44 +0000

    Save the worker from unhandled exceptions
    Without this the worker would exit in the included testcase.
    
    (cherry picked from commit 43806322c083e2bbb37bbdd5913d4be5ae0e8a13)

--- api/src/worker.ts
+++ api/src/worker.ts
@@ -8,6 +8,15 @@ import * as Chisel from "./chisel.ts";
 // so we need to force typescript to accept this.
 globalThis.window = globalThis as unknown as (Window & typeof globalThis);
 
+Deno.core.setPromiseRejectCallback((_type, _promise, reason) => {
+    // Without this function deno pushes the exception to
+    // pending_promise_exceptions, which eventually causes an unlucky
+    // user of poll_event_loop to get an error. Since user code can
+    // create and reject a promise that lacks a handler, we have to do
+    // this. Throwing in here causes deno to at least log the stack.
+    throw new Error("Promise rejected without a handler: " + reason);
+});
+
 type requestHandler = (req: Request) => Promise<Response>;
 // Handlers that have been compiled but are not yet serving
 // requests. The function activateEndpoint moves handler from
