4942ef4677875c0f2f8070abfb8695db759c64ea
Pekka Enberg <penberg@chiselstrike.com>
2022-04-22 09:23:12 +0300

    cli: Kill `chiseld` on `chisel dev` termination
    The `chisel dev` command forks `chiseld` in the background. Make sure
    that we also kill `chiseld` when `chisel dev` terminates; otherwise it's
    hard to use `chisel dev` in shell scripts, for example, is pretty hard.

--- cli/src/cmd/dev.rs
+++ cli/src/cmd/dev.rs
@@ -6,23 +6,43 @@ use crate::server::{start_server, wait};
 use crate::DEFAULT_API_VERSION;
 use anyhow::Result;
 use futures::channel::mpsc::channel;
-use futures::{SinkExt, StreamExt};
+use futures::{FutureExt, SinkExt, StreamExt};
 use notify::{event::ModifyKind, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use std::collections::HashSet;
+use std::panic;
 use std::path::Path;
 use std::path::PathBuf;
 use std::time::Duration;
+use tokio::task::JoinHandle;
 
 pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> {
     let type_check = type_check.into();
     let manifest = read_manifest()?;
+    let mut sigterm = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?;
+    let mut sigint = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())?;
+    let mut sighup = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::hangup())?;
+    let default_hook = panic::take_hook();
+    panic::set_hook(Box::new(move |info| {
+        default_hook(info);
+        nix::sys::signal::raise(nix::sys::signal::Signal::SIGINT).unwrap();
+    }));
+    let (mut signal_tx, mut signal_rx) = channel(1);
+    let sig_task: JoinHandle<Result<()>> = tokio::task::spawn(async move {
+        let _ = futures::select! {
+            _ = sigterm.recv().fuse() => { },
+            _ = sigint.recv().fuse() => { },
+            _ = sighup.recv().fuse() => { },
+        };
+        signal_tx.send(()).await?;
+        Ok(())
+    });
     let mut server = start_server()?;
     wait(server_url.clone()).await?;
     apply_from_dev(server_url.clone(), type_check).await;
-    let (mut tx, mut rx) = channel(1);
+    let (mut watcher_tx, mut watcher_rx) = channel(1);
     let mut apply_watcher = RecommendedWatcher::new(move |res: Result<Event, notify::Error>| {
         futures::executor::block_on(async {
-            tx.send(res).await.unwrap();
+            watcher_tx.send(res).await.unwrap();
         });
     })?;
     let watcher_config = notify::Config::OngoingEvents(Some(Duration::from_millis(100)));
@@ -39,28 +59,38 @@ pub(crate) async fn cmd_dev(server_url: String, type_check: bool) -> Result<()> 
         let policies_dir = Path::new(policies_dir);
         apply_watcher.watch(policies_dir, RecursiveMode::Recursive)?;
     }
-    while let Some(res) = rx.next().await {
-        match res {
-            Ok(Event {
-                kind: EventKind::Modify(ModifyKind::Data(_) | ModifyKind::Name(_)),
-                paths,
-                ..
-            }) => {
-                let paths: HashSet<PathBuf> = HashSet::from_iter(
-                    paths
-                        .into_iter()
-                        .filter(|path| !crate::project::ignore_path(path.to_str().unwrap())),
-                );
-                let paths: HashSet<PathBuf> = HashSet::from_iter(paths.into_iter());
-                if !paths.is_empty() {
-                    apply_from_dev(server_url.clone(), type_check).await;
+    loop {
+        futures::select! {
+            _ = signal_rx.next().fuse() => {
+                break;
+            }
+            res = watcher_rx.next().fuse() => {
+                let res = res.unwrap();
+                match res {
+                    Ok(Event {
+                        kind: EventKind::Modify(ModifyKind::Data(_) | ModifyKind::Name(_)),
+                        paths,
+                        ..
+                    }) => {
+                        let paths: HashSet<PathBuf> = HashSet::from_iter(
+                            paths
+                                .into_iter()
+                                .filter(|path| !crate::project::ignore_path(path.to_str().unwrap())),
+                        );
+                        let paths: HashSet<PathBuf> = HashSet::from_iter(paths.into_iter());
+                        if !paths.is_empty() {
+                            apply_from_dev(server_url.clone(), type_check).await;
+                        }
+                    }
+                    Ok(_) => { /* ignore */ }
+                    Err(e) => println!("watch error: {:?}", e),
                 }
             }
-            Ok(_) => { /* ignore */ }
-            Err(e) => println!("watch error: {:?}", e),
         }
     }
+    server.kill()?;
     server.wait()?;
+    sig_task.await??;
 
     Ok(())
 }
