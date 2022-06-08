51c3fa7018a6c619dca7b301f2be6a1e5db96caa
Pekka Enberg <penberg@chiselstrike.com>
2022-06-08 16:41:46 +0300

    chiselc: Unify error handling in extract_filter()

--- chiselc/src/transforms/utils.rs
+++ chiselc/src/transforms/utils.rs
@@ -75,9 +75,7 @@ pub fn extract_filter(
                 Some(expr) => match &**expr {
                     Expr::Bin(bin_expr) => convert_bin_expr(bin_expr),
                     Expr::Lit(Lit::Bool(value)) => Ok(QExpr::Literal(QLiteral::Bool(value.value))),
-                    _ => {
-                        todo!();
-                    }
+                    _ => todo!("Unsupported filter predicate expression: {:?}", expr),
                 },
                 None => {
                     todo!();
