cf07f44191c8cc296af6821cccce19c4acd5d8c9
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-06-15 16:11:04 +0000

    Upgrade deno

--- api/src/worker.ts
+++ api/src/worker.ts
@@ -8,16 +8,19 @@ import * as Chisel from "./chisel.ts";
 // so we need to force typescript to accept this.
 globalThis.window = globalThis as unknown as (Window & typeof globalThis);
 
-Deno.core.setPromiseRejectCallback((type, _promise, reason) => {
-    if (type == 0) { // PromiseRejectWithNoHandler
-        // Without this function deno pushes the exception to
-        // pending_promise_exceptions, which eventually causes an unlucky
-        // user of poll_event_loop to get an error. Since user code can
-        // create and reject a promise that lacks a handler, we have to do
-        // this. Throwing in here causes deno to at least log the stack.
-        throw new Error("Promise rejected without a handler: " + reason);
-    }
-});
+Deno.core.opSync(
+    "op_set_promise_reject_callback",
+    (type: number, _promise: unknown, reason: unknown) => {
+        if (type == 0) { // PromiseRejectWithNoHandler
+            // Without this function deno pushes the exception to
+            // pending_promise_exceptions, which eventually causes an unlucky
+            // user of poll_event_loop to get an error. Since user code can
+            // create and reject a promise that lacks a handler, we have to do
+            // this. Throwing in here causes deno to at least log the stack.
+            throw new Error("Promise rejected without a handler: " + reason);
+        }
+    },
+);
 
 type requestHandler = (req: Request) => Promise<Response>;
 // Handlers that have been compiled but are not yet serving
