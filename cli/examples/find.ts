ce0708ff76fcc64054f39a10f3673d72bc0307da
Rafael Ávila de Espíndola <espindola@chiselstrike.com>
2022-06-16 10:13:27 +0000

    Convert find.js to TypeScript

--- cli/examples/find.ts
+++ cli/examples/find.ts
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: © 2021 ChiselStrike <info@chiselstrike.com>
-export default async function chisel(req) {
+import { Person } from "../models/person.ts";
+
+export default async function chisel(req: Request) {
     let response = "";
     for await (let person of Person.cursor()) {
         let fields = [person.first_name, person.last_name, person.age, person.human, person.height];
