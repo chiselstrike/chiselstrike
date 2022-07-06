17d4d80cb43b61c0db8fff2f9b7e865c16b8f981
Marin Postma <postma.marin@protonmail.com>
2022-07-06 13:16:17 +0200

    chisel start: exit on early chiseld return

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -10,6 +10,7 @@ use chisel::{
     type_msg::TypeEnum, ChiselDeleteRequest, DescribeRequest, PopulateRequest, RestartRequest,
     StatusRequest,
 };
+use futures::{pin_mut, FutureExt};
 use std::env;
 use std::fs;
 use std::io::ErrorKind;
@@ -262,8 +263,21 @@ async fn main() -> Result<()> {
         }
         Command::Start => {
             let mut server = start_server(chiseld_args)?;
-            wait(server_url).await?;
-            server.wait().await?;
+            let url_fut = wait(server_url).fuse();
+
+            pin_mut!(url_fut);
+
+            loop {
+                tokio::select! {
+                    res = server.wait() => {
+                        res?;
+                        break;
+                    }
+                    res = &mut url_fut => {
+                        res?;
+                    }
+                }
+            }
         }
         Command::Status => {
             let mut client = ChiselRpcClient::connect(server_url).await?;
