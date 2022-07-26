6c23043674bbdedfd5d7501224427a950b9d3328
Glauber Costa <glauber@chiselstrike.com>
2022-07-26 16:01:22 -0400

    simplify hello endpoint
    Since chiselstrike 0.11, this code can be simpler.

--- packages/create-chiselstrike-app/template/hello.ts
+++ packages/create-chiselstrike-app/template/hello.ts
@@ -4,7 +4,6 @@
 //
 // curl -d '{"hello": "world"}' localhost:8080/dev/hello
 
-export default async function (req: Request): Promise<Response> {
-    const response = await req.text() || "hello world";
-    return new Response(response);
+export default async function (req: Request): Promise<string> {
+    return await req.text() || "hello world";
 }
