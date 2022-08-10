5bddc39b12da8e68071bc93e674fc5538c2e3f4a
Pekka Enberg <penberg@chiselstrike.com>
2022-08-10 14:06:21 +0300

    tests: Simplify integration tests
    Simplify integration tests by introducing a macro for defining an
    integration test to run in `all_tests()`.

--- cli/tests/integration_tests/rust_tests/mod.rs
+++ cli/tests/integration_tests/rust_tests/mod.rs
@@ -4,22 +4,36 @@ pub mod test_bad_filter;
 pub mod test_find_by;
 pub mod test_http_import;
 
+use test_bad_filter::*;
+use test_find_by::*;
+use test_http_import::*;
+
+macro_rules! test {
+    ($func_name:ident, $mode:expr) => {
+        IntegrationTest {
+            name: stringify!($func_name),
+            mode: $mode,
+            test_fn: &$func_name,
+        }
+    };
+}
+
+macro_rules! deno_test {
+    ($func_name:ident) => {
+        test!($func_name, OpMode::Deno)
+    };
+}
+
+macro_rules! node_test {
+    ($func_name:ident) => {
+        test!($func_name, OpMode::Node)
+    };
+}
+
 pub fn all_tests() -> Vec<IntegrationTest> {
     vec![
-        IntegrationTest {
-            name: "test_bad_filter",
-            mode: OpMode::Deno,
-            test_fn: &test_bad_filter::test_bad_filter,
-        },
-        IntegrationTest {
-            name: "test_find_by",
-            mode: OpMode::Deno,
-            test_fn: &test_find_by::test_find_by,
-        },
-        IntegrationTest {
-            name: "test_http_import",
-            mode: OpMode::Node,
-            test_fn: &test_http_import::test_http_import,
-        },
+        deno_test!(test_bad_filter),
+        node_test!(test_find_by),
+        node_test!(test_http_import),
     ]
 }
