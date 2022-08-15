2378f782b4c5147f41e02b84bafc1bdc7395e334
Pekka Enberg <penberg@chiselstrike.com>
2022-08-15 10:01:22 +0300

    tests: Simple 'chisel apply' test for Kafka
    This only tests that event handlers are picked up, not that they consume
    from a Kafka topic.

---
+++ cli/tests/integration_tests/rust_tests/test_kafka.rs
@@ -0,0 +1,17 @@
+use crate::framework::prelude::*;
+
+#[chisel_macros::test(modules = Node)]
+pub async fn test_kafka_apply(c: TestContext) {
+    c.chisel.write(
+        "events/test-topic.ts",
+        r##"
+        import { ChiselEvent } from "@chiselstrike/api";
+
+        export default async function (event: ChiselEvent) {
+            console.log(event);
+        }
+    "##,
+    );
+
+    c.chisel.apply().await.expect("Event handler defined: /dev/test-topic");
+}
