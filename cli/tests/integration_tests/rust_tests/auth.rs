796ef59db6e98d5efb57c6fa9afb55fd41492ffa
Jan Plhak <jp@chiselstrike.com>
2022-08-25 16:08:21 +0200

    Rewrite next-auth-crud.node test to Rust.

---
+++ cli/tests/integration_tests/rust_tests/auth.rs
@@ -0,0 +1,77 @@
+use crate::framework::prelude::*;
+
+#[chisel_macros::test(modules = Node)]
+pub async fn next_auth_crud(mut c: TestContext) {
+    c.chisel
+        .write(".env", r##"{ "CHISELD_AUTH_SECRET" : "1234" }"##);
+
+    c.restart_chiseld().await;
+    c.chisel.apply().await.unwrap();
+
+    // Test that we get forbidden if we provide invalid auth header.
+    c.chisel
+        .post("/__chiselstrike/auth/users")
+        .json(json!({"name":"Foo", "email":"foo@t.co"}))
+        .header("ChiselAuth", "123")
+        .send()
+        .await
+        .assert_status(403);
+
+    {
+        // Create a new auth user
+        c.chisel
+            .post("/__chiselstrike/auth/users")
+            .json(json!({"name":"Foo", "email":"foo@t.co"}))
+            .header("ChiselAuth", "1234")
+            .send()
+            .await
+            .assert_ok();
+
+        // Delete the user
+        c.chisel
+            .delete("/__chiselstrike/auth/users?.name=Foo")
+            .header("ChiselAuth", "1234")
+            .send()
+            .await
+            .assert_ok();
+
+        // Check it's not there anymore
+        c.chisel
+            .get("/__chiselstrike/auth/users")
+            .header("ChiselAuth", "1234")
+            .send()
+            .await
+            .assert_json(json!({"results": []}));
+    }
+
+    c.chisel
+        .post("/__chiselstrike/auth/sessions")
+        .json(json!({"sessionToken":"tok1", "userId":"id1", "expires":"2025-12-31"}))
+        .header("ChiselAuth", "1234")
+        .send()
+        .await
+        .assert_ok();
+
+    c.chisel
+        .post("/__chiselstrike/auth/tokens")
+        .json(json!({"token":"tok1", "identifier":"id1", "expires":"2026-12-31"}))
+        .header("ChiselAuth", "1234")
+        .send()
+        .await
+        .assert_ok();
+
+    c.chisel
+        .post("/__chiselstrike/auth/accounts")
+        .json(json!({
+            "providerAccountId":"acct1",
+            "userId":"usr1",
+            "provider":"gh",
+            "type":"oauth",
+            "expires_at":42,
+            "session_state":"good"
+        }))
+        .header("ChiselAuth", "1234")
+        .send()
+        .await
+        .assert_ok();
+}
