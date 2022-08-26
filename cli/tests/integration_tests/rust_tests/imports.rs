373fdd1cf2f90119178ab7d2609065d5e66cfb9b
Jan Plhak <jp@chiselstrike.com>
2022-08-26 10:47:38 +0200

    Rewrite test use-ts-muduled.deno to rust.

--- cli/tests/integration_tests/rust_tests/imports.rs
+++ cli/tests/integration_tests/rust_tests/imports.rs
@@ -1,7 +1,7 @@
 use crate::framework::prelude::*;
 
 #[chisel_macros::test(modules = Node)]
-pub async fn test(c: TestContext) {
+pub async fn http(c: TestContext) {
     c.chisel.write(
         "routes/error.ts",
         r##"
@@ -18,4 +18,31 @@ pub async fn test(c: TestContext) {
         .stderr
         .read("Could not apply the provided code")
         .read("chiseld cannot load module https://foo.bar/ at runtime");
+}
+
+#[chisel_macros::test(modules = Deno)]
+pub async fn deno_land_module(mut c: TestContext) {
+    c.chisel.write(
+        "routes/indented.ts",
+        r##"
+        import indent from 'https://deno.land/x/text_indent@v0.1.0/mod.ts';
+
+        export default async function chisel(req: Request) {
+            return "test" + indent("foo", 4);
+        }
+    "##,
+    );
+
+    c.chisel.apply().await.unwrap();
+    assert_eq!(
+        c.chisel.get_text("/dev/indented").await,
+        "test    foo"
+    );
+
+    // Try after restart as well
+    c.restart_chiseld().await;
+    assert_eq!(
+        c.chisel.get_text("/dev/indented").await,
+        "test    foo"
+    );
 }
