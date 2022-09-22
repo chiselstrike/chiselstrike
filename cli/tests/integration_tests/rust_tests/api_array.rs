5c2c537fe65b93974ce81c5430d61e6c8a129ef7
Jan Plhak <jp@chiselstrike.com>
2022-09-22 16:13:59 +0200

    Add support for arrays of Dates.

--- cli/tests/integration_tests/rust_tests/api_array.rs
+++ cli/tests/integration_tests/rust_tests/api_array.rs
@@ -13,6 +13,7 @@ pub async fn test(c: TestContext) {
             numbers: number[] = [];
             strings: string[] = [];
             booleans: boolean[][] = [];
+            dates: Date[] = [];
         }
     "##,
     );
@@ -25,7 +26,8 @@ pub async fn test(c: TestContext) {
                 order: 0,
                 numbers: [1, 0, -1.01],
                 strings: ["Hello", "World"],
-                booleans: [[false], [true, false]]
+                booleans: [[false], [true, false]],
+                dates: [new Date(1662624988000)]
             }).save();
         }
     "##,
@@ -58,6 +60,7 @@ pub async fn test(c: TestContext) {
         assert_eq!(entity["numbers"], json!([1, 0, -1.01]));
         assert_eq!(entity["strings"], json!(["Hello", "World"]));
         assert_eq!(entity["booleans"], json!([[false], [true, false]]));
+        assert_eq!(entity["dates"], json!([1662624988000i64]));
     }
     {
         let data = c.chisel.get_json("/dev/crud_foos").await;
@@ -66,6 +69,7 @@ pub async fn test(c: TestContext) {
         assert_eq!(entity["numbers"], json!([1, 0, -1.01]));
         assert_eq!(entity["strings"], json!(["Hello", "World"]));
         assert_eq!(entity["booleans"], json!([[false], [true, false]]));
+        assert_eq!(entity["dates"], json!([1662624988000i64]));
     }
 
     c.chisel
@@ -75,7 +79,8 @@ pub async fn test(c: TestContext) {
                 "order": 1,
                 "numbers": [0, 1, 2],
                 "strings": ["Sauna", "rlz"],
-                "booleans": [[true], [false]]
+                "booleans": [[true], [false]],
+                "dates": [42]
             }),
         )
         .await;
@@ -86,55 +91,67 @@ pub async fn test(c: TestContext) {
         assert_eq!(entity0["numbers"], json!([1, 0, -1.01]));
         assert_eq!(entity0["strings"], json!(["Hello", "World"]));
         assert_eq!(entity0["booleans"], json!([[false], [true, false]]));
+        assert_eq!(entity0["dates"], json!([1662624988000i64]));
 
         let entity1 = &data["results"][1];
         assert_eq!(entity1["order"], json!(1));
         assert_eq!(entity1["numbers"], json!([0, 1, 2]));
         assert_eq!(entity1["strings"], json!(["Sauna", "rlz"]));
         assert_eq!(entity1["booleans"], json!([[true], [false]]));
+        assert_eq!(entity1["dates"], json!([42]));
     }
 
     {
-        let status = c
-            .chisel
-            .post_json_status(
-                "/dev/crud_foos",
-                json!({
-                    "order": 1,
-                    "numbers": ["1"],
-                    "strings": ["correct"],
-                    "booleans": [[true]]
-                }),
-            )
-            .await;
-        assert_eq!(status, 500);
+        c.chisel
+            .post("/dev/crud_foos")
+            .json(json!({
+                "order": 1,
+                "numbers": ["1"],
+                "strings": ["correct"],
+                "booleans": [[true]],
+                "dates": [42]
+            }))
+            .send()
+            .await
+            .assert_status(500);
 
-        let status = c
-            .chisel
-            .post_json_status(
-                "/dev/crud_foos",
-                json!({
-                    "order": 1,
-                    "numbers": [1],
-                    "strings": [true],
-                    "booleans": [[true]]
-                }),
-            )
-            .await;
-        assert_eq!(status, 500);
+        c.chisel
+            .post("/dev/crud_foos")
+            .json(json!({
+                "order": 1,
+                "numbers": [1],
+                "strings": [true],
+                "booleans": [[true]],
+                "dates": [42]
+            }))
+            .send()
+            .await
+            .assert_status(500);
 
-        let status = c
-            .chisel
-            .post_json_status(
-                "/dev/crud_foos",
-                json!({
-                    "order": 1,
-                    "numbers": [1],
-                    "strings": ["correct"],
-                    "booleans": [[1]]
-                }),
-            )
-            .await;
-        assert_eq!(status, 500);
+        c.chisel
+            .post("/dev/crud_foos")
+            .json(json!({
+                "order": 1,
+                "numbers": [1],
+                "strings": ["correct"],
+                "booleans": [[1]],
+                "dates": [42]
+            }))
+            .send()
+            .await
+            .assert_status(500);
+
+        c.chisel
+            .post("/dev/crud_foos")
+            .json(json!({
+                "order": 1,
+                "numbers": [1],
+                "strings": ["correct"],
+                "booleans": [[true]],
+                "dates": ["foo"]
+            }))
+            .send()
+            .await
+            .assert_status(500);
     }
 }
