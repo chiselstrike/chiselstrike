b53b92ce4855e08b0c9e27b1085749129bebb5a9
Pekka Enberg <penberg@chiselstrike.com>
2022-10-13 08:18:48 +0200

    ChiselStrike adapter for NextAuth.js
    This adds a ChiselStrike adapter for NextAuth.js. The adapter is based
    on the work Dejan did earlier:
    
    https://github.com/nextauthjs/next-auth/pull/4573
    
    The package is standalone and, therefore, does not require NextAuth.js
    to integrate our work into their tree for our users to use this.
    
    Please note that although you can't run the tests in the tree, they do
    pass if you integrate the package with the NextAuth.js repository and
    enable the "workspace" tagged dependencies (that only pnpm understands).

---
+++ packages/chiselstrike-next-auth/jest.config.js
@@ -0,0 +1,7 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+module.exports = {
+    ...require("@next-auth/adapter-test/jest/jest-preset"),
+};
---
+++ packages/chiselstrike-next-auth/lib/index.ts
@@ -0,0 +1,1 @@
+export { ChiselAdapter } from "./adapter";
