70ce08a38b72570f96ef4184c135f5731bae3510
Jan Plhak <jp@chiselstrike.com>
2022-11-02 15:13:36 +0100

    Add an integration test checking that chiselc is working.

---
+++ cli/tests/integration_tests/rust_tests/chiselc.rs
@@ -0,0 +1,35 @@
+use crate::framework::prelude::*;
+
+#[chisel_macros::test(modules = Deno, optimize = Yes)]
+pub async fn check_optimization(c: TestContext) {
+    c.chisel.write(
+        "models/person.ts",
+        r#"
+        import { ChiselEntity } from "@chiselstrike/api";
+
+        export class Person extends ChiselEntity {
+            name: string = "";
+        }
+    "#,
+    );
+    c.chisel.write(
+        "routes/query.ts",
+        r#"
+        import { ChiselRequest } from '@chiselstrike/api';
+        import { Person } from "../models/person.ts";
+
+        export default async function chisel(req: ChiselRequest) {
+            const c = Person.cursor()
+                .filter(p => p.name == "Peter")
+            // @ts-ignore: We need to access the inner field
+            if (c.inner.type != "InternalFilter") {
+                throw Error("Chisel compiler didn't optimize filtering query");
+            }
+            return await c
+                .map(p => p.name)
+                .toArray();
+        }"#,
+    );
+    c.chisel.apply_ok().await;
+    c.chisel.get("/dev/query").send().await.assert_ok();
+}
