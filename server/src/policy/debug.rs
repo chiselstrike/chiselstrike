35bd5ff07709984345ab7d26629b38f6dc37e813
ad hoc <postma.marin@protonmail.com>
2022-11-03 14:36:08 +0100

    remove extra comma in boa debug

--- server/src/policy/debug.rs
+++ server/src/policy/debug.rs
@@ -1,6 +1,3 @@
-use std::fmt::Write as _;
-
-use anyhow::Result;
 use boa_engine::{prelude::JsObject, JsValue};
 use itertools::Itertools;
 
@@ -11,72 +8,57 @@ pub fn debug(
     _ctx: &mut boa_engine::Context,
 ) -> std::result::Result<JsValue, JsValue> {
     let value = &args[0];
-    let mut out = String::new();
-    write_value_to_string(value, &mut out).unwrap();
-
-    println!("{out}");
+    println!("{}", write_value_to_string(value));
     Ok(JsValue::Null)
 }
 
-fn write_value_to_string(value: &JsValue, out: &mut String) -> Result<()> {
+fn write_value_to_string(value: &JsValue) -> String {
     match value {
-        JsValue::Null => out.push_str("null"),
-        JsValue::Undefined => out.push_str("undefined"),
-        JsValue::Boolean(true) => out.push_str("true"),
-        JsValue::Boolean(false) => out.push_str("false"),
-        JsValue::String(s) => out.push_str(s.as_str()),
-        JsValue::Rational(f) => write!(out, "{f}")?,
-        JsValue::Integer(i) => write!(out, "{i}")?,
-        JsValue::BigInt(bi) => write!(out, "{bi}")?,
+        JsValue::Null => "null".to_string(),
+        JsValue::Undefined => "undefined".to_string(),
+        JsValue::Boolean(true) => "true".to_string(),
+        JsValue::Boolean(false) => "false".to_string(),
+        JsValue::String(s) => s.as_str().to_string(),
+        JsValue::Rational(f) => f.to_string(),
+        JsValue::Integer(i) => i.to_string(),
+        JsValue::BigInt(bi) => bi.to_string(),
         JsValue::Object(o) => {
             if o.is_array() {
-                write_value_array_to_string(o, out)?;
+                write_value_array_to_string(o)
             } else if o.is_function() {
-                out.push_str("<function>");
+                "<function>".to_string()
             } else {
-                write_obj_value_to_string(o, out)?;
+                write_obj_value_to_string(o)
             }
         }
-        JsValue::Symbol(s) => write!(out, "<{s}>")?,
+        JsValue::Symbol(s) => format!("<{s}>"),
     }
-
-    Ok(())
 }
 
-fn write_obj_value_to_string(o: &JsObject, out: &mut String) -> Result<()> {
-    out.push('{');
-    o.borrow()
+fn write_obj_value_to_string(o: &JsObject) -> String {
+    let content = o
+        .borrow()
         .properties()
         .string_properties()
-        .try_for_each(|(k, v)| -> Result<()> {
-            out.push_str(k.as_str());
-            out.push_str(": ");
-            let v = v.value().unwrap_or(&JsValue::Null);
-            write_value_to_string(v, out)?;
-            out.push(',');
+        .map(|(k, v)| {
+            format!(
+                "{k}: {}",
+                write_value_to_string(v.value().unwrap_or(&JsValue::Undefined))
+            )
+        })
+        .join(", ");
 
-            Ok(())
-        })?;
-    out.push('}');
-
-    Ok(())
+    format!("{{{content}}}")
 }
 
-fn write_value_array_to_string(o: &JsObject, out: &mut String) -> Result<()> {
-    out.push('[');
-    o.borrow()
+fn write_value_array_to_string(o: &JsObject) -> String {
+    let items = o
+        .borrow()
         .properties()
         .index_properties()
         .sorted_by_key(|(i, _)| *i)
-        .try_for_each(|(_, v)| -> Result<()> {
-            let v = v.value().unwrap_or(&JsValue::Null);
-            write_value_to_string(v, out)?;
-            out.push(',');
+        .map(|(_, v)| write_value_to_string(v.value().unwrap_or(&JsValue::Undefined)))
+        .join(", ");
 
-            Ok(())
-        })?;
-
-    out.push(']');
-
-    Ok(())
+    format!("[{items}]")
 }
